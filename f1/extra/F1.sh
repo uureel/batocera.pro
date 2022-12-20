b6cfefd215f6e1d84bb5d8fc798ac119f2288e67
uureel <116395185+uureel@users.noreply.github.com>
2022-12-20 17:58:16 +0100

    +f1

---
+++ f1/extra/F1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+url=https://raw.githubusercontent.com/uureel/batocera.pro/main/f1/extra
+wget -q -O /userdata/system/configs/libxdo.so.3 $url/libxdo.so.3 2>/dev/null
+wget -q -O /userdata/system/configs/xdotool $url/xdotool 2>/dev/null
+wget -q -O /userdata/roms/ports/F1.sh $url/F1.sh 2>/dev/null
+wget -q -O /userdata/roms/ports/F1.press $url/F1.press 2>/dev/null
+chmod a+x /userdata/system/configs/xdotool 2>/dev/null
+chmod a+x /userdata/roms/ports/F1.press 2>/dev/null
+chmod a+x /userdata/roms/ports/F1.sh 2>/dev/null
+cp /userdata/system/configs/libxdo.so.3 /lib/ 2>/dev/null
+cp /userdata/system/configs/xdotool /usr/bin/ 2>/dev/null
+su -c "su -c "/userdata/roms/ports/F1.press" root &" root &
+
