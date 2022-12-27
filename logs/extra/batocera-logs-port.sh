83b0396f73c19e41490be778b5fbf7f8e119deca
uureel <116395185+uureel@users.noreply.github.com>
2022-12-27 20:10:58 +0100

    Update batocera-logs-port.sh

--- logs/extra/batocera-logs-port.sh
+++ logs/extra/batocera-logs-port.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DISPLAY=:0.0 xterm -en en_US.UTF-8 -u8 -bg black -fg white -fa Monospace -fs 4 -sb -e bash -c "/usr/bin/logs 2>/dev/null" 2>/dev/null &
+DISPLAY=:0.0 xterm -en en_US.UTF-8 -u8 -bg black -fg white -fa Monospace -fs 5 -sb -e bash -c "/usr/bin/logs 2>/dev/null" 2>/dev/null &
