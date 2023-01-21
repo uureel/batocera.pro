31d54ad65122658eaeb23eb7a73ebd06d44a22f2
uureel <116395185+uureel@users.noreply.github.com>
2023-01-21 15:47:57 +0100

    +++++++++

---
+++ sunshine/installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##################################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+app=sunshine
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------
+##################################################################################################################################
+cd /tmp/ ; rm "/tmp/pro-framework.sh" 2>/dev/null ; rm "/tmp/$app.sh" 2>/dev/null ;
+wget --no-cache -q -O "/tmp/pro-framework.sh" "https://raw.githubusercontent.com/uureel/batocera.pro/main/.dep/pro-framework.sh" ; 
+wget --no-cache -q -O "/tmp/$app.sh" "https://raw.githubusercontent.com/uureel/batocera.pro/main/$app/$app.sh" ; 
+dos2unix /tmp/pro-framework.sh ; dos2unix "/tmp/$app.sh" ;  
+source /tmp/pro-framework.sh  
+run-xterm "/tmp/$app.sh"
+# batocera.pro // 
