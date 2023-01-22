ef743f0d64a9eb470d74541aa3e6b3145285b4f5
uureel <116395185+uureel@users.noreply.github.com>
2023-01-22 04:53:31 +0100

    Update installer.sh

--- sunshine/installer.sh
+++ sunshine/installer.sh
@@ -12,7 +12,7 @@ app=sunshine
 
 #---------------------------------------------------------------------------------------------------------------------------------
 ##################################################################################################################################
-cd /tmp/ ; rm "/tmp/pro-framework.sh" 2>/dev/null ; rm "/tmp/$app.sh" 2>/dev/null ;
+export DISPLAY=:0.0 ; cd /tmp/ ; rm "/tmp/pro-framework.sh" 2>/dev/null ; rm "/tmp/$app.sh" 2>/dev/null ;
 wget --no-cache -q -O "/tmp/pro-framework.sh" "https://raw.githubusercontent.com/uureel/batocera.pro/main/.dep/pro-framework.sh" ; 
 wget --no-cache -q -O "/tmp/$app.sh" "https://raw.githubusercontent.com/uureel/batocera.pro/main/$app/$app.sh" ; 
 dos2unix /tmp/pro-framework.sh ; dos2unix "/tmp/$app.sh" ;  
