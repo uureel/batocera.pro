8b2dc3365f4dd46c8f8c65767ade65abaf2be648
uureel <116395185+uureel@users.noreply.github.com>
2023-01-26 21:39:56 +0100

    Update custom.sh

--- sunshine/extras/custom.sh
+++ sunshine/extras/custom.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 mkdir -p /userdata/system/.config/$app
 cp /userdata/system/pro/sunshine/extras/apps.json /userdata/system/.config/$app/
 cp /userdata/system/pro/sunshine/extras/sunshine.conf /userdata/system/.config/sunshine/
+
+dos2unix /userdata/system/pro/sunshine/extras/startup.sh 
+chmod a+x /userdata/system/pro/sunshine/extras/startup.sh 
+/userdata/system/pro/sunshine/extras/startup.sh 
