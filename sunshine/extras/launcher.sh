b9b27800a86057a63efcb2f39a02301c176f2f03
uureel <116395185+uureel@users.noreply.github.com>
2023-01-26 21:45:52 +0100

    Update launcher.sh

--- sunshine/extras/launcher.sh
+++ sunshine/extras/launcher.sh
@@ -11,11 +11,11 @@ rm $log1 2>/dev/null ; rm $log2 2>/dev/null
 if [[ "$(ls /usr/share/applications/ | grep "sunshined.desktop")" != "" ]]; then 
         rm /usr/share/applications/sunshined.desktop 2>/dev/null 
         rm /usr/share/applications/sunshine.desktop 2>/dev/null 
-        cp /userdata/system/pro/sunshine/extra/sunshine.desktop /usr/share/applications/ 
+        cp /userdata/system/pro/sunshine/extras/sunshine.desktop /usr/share/applications/ 
 elif [[ "$(ls /usr/share/applications/ | grep "sunshine.desktop")" != "" ]]; then 
         rm /usr/share/applications/sunshined.desktop 2>/dev/null 
         rm /usr/share/applications/sunshine.desktop 2>/dev/null 
-        cp /userdata/system/pro/sunshine/extra/sunshined.desktop /usr/share/applications/ 
+        cp /userdata/system/pro/sunshine/extras/sunshined.desktop /usr/share/applications/ 
 fi 
 
 killall -9 sunshine 1>/dev/null 2>/dev/null & sleep 1 
