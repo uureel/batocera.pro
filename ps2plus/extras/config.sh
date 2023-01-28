c6baba58f3a4bb14a6f4d77f4a8dc8667526ae35
uureel <116395185+uureel@users.noreply.github.com>
2023-01-28 04:41:35 +0100

    +++++++++

--- ps2plus/extras/config.sh
+++ ps2plus/extras/config.sh
@@ -17,16 +17,16 @@ chmod a+x /userdata/system/pro/ps2plus/pcsx2/pcsx2-SSE4.AppImage
 
 # start appimage: 
 if [[ "$(cat /tmp/cpufeatures | grep avx2)" != "" ]]; then 
-	if [[ "$(echo "$ROM" | grep "RPCS2" | grep "CONFIG")" != "" ]] || [[ "$(echo "ROM")" = "" ]]; then
+	if [[ "$(echo "$ROM" | grep "CONFIG")" != "" ]] || [[ "$(echo "ROM")" = "" ]]; then
 	unclutter-remote -s; DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=1 /userdata/system/pro/ps2plus/pcsx2/pcsx2-AVX2.AppImage 1>$log1 2>$log2
 	else 
-	DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=1 /userdata/system/pro/ps2plus/pcsx2/pcsx2-AVX2.AppImage -fullscreen -nogui "$ROM" 1>$log1 2>$log2
+	DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=1 /userdata/system/pro/ps2plus/pcsx2/pcsx2-AVX2.AppImage -fullscreen "$ROM" 1>$log1 2>$log2
 	fi
 else 
-	if [[ "$(echo "$ROM" | grep "RPCS2" | grep "CONFIG")" != "" ]] || [[ "$(echo "$ROM")" = "" ]]; then
+	if [[ "$(echo "$ROM" | grep "CONFIG")" != "" ]] || [[ "$(echo "$ROM")" = "" ]]; then
 	unclutter-remote -s; DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=1 /userdata/system/pro/ps2plus/pcsx2/pcsx2-SSE4.AppImage 1>$log1 2>$log2
 	else 
-	DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=1 /userdata/system/pro/ps2plus/pcsx2/pcsx2-SSE4.AppImage -fullscreen -nogui "$ROM" 1>$log1 2>$log2
+	DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=1 /userdata/system/pro/ps2plus/pcsx2/pcsx2-SSE4.AppImage -fullscreen "$ROM" 1>$log1 2>$log2
 	fi 
 fi
 
