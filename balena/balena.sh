e7660a0722f3897257fe443224b098d44962b0cd
uureel <116395185+uureel@users.noreply.github.com>
2023-03-27 03:09:32 +0200

    +

--- balena/balena.sh
+++ balena/balena.sh
@@ -18,11 +18,11 @@
 APPNAME="BALENA-ETCHER"
 appname=balena
 # 
-APPLINK=https://apprepo.de/appimage/download/etcher
-APPHOME="apprepo.de/appimage/etcher"
+APPLINK=https://github.com/uureel/batocera-apps/raw/main/apps/balena-etcher/balena-etcher.AppImage
+APPHOME="github.com/uureel/batocera-apps"
 #---------------------------------------------------------------------
 # DEFINE LAUNCHER COMMAND >> 
-COMMAND='mkdir /userdata/system/pro/'$appname'/home 2>/dev/null; mkdir /userdata/system/pro/'$appname'/config 2>/dev/null; mkdir /userdata/system/pro/'$appname'/roms 2>/dev/null; HOME=/userdata/system/pro/'$appname'/home XDG_CONFIG_HOME=/userdata/system/pro/'$appname'/config QT_SCALE_FACTOR="1" GDK_SCALE="1" XDG_DATA_HOME=/userdata/system/pro/'$appname'/home DISPLAY=:0.0 /userdata/system/pro/'$appname'/'$appname'.AppImage'
+COMMAND='DISPLAY=:0.0 /userdata/system/pro/'$appname'/'$appname'.AppImage'
 #---------------------------------------------------------------------
 ######################################################################
 ######################################################################
