999b1ccaa1d5aee175bb7ad464fb5d6af84a8617
uureel <116395185+uureel@users.noreply.github.com>
2023-03-30 10:45:00 +0200

    +

--- sunshine/extras/startup.sh
+++ sunshine/extras/startup.sh
@@ -8,11 +8,13 @@ log1=/userdata/system/pro/sunshine/log1.txt
 log2=/userdata/system/pro/sunshine/log2.txt
 rm $log1 2>/dev/null ; rm $log2 2>/dev/null 
 
-cp /userdata/system/pro/sunshine/extras/sunshine.desktop /usr/share/applications/ 2>/dev/null ; sleep 10 
+cp /userdata/system/pro/sunshine/extras/sunshine.desktop /usr/share/applications/ 2>/dev/null 
 
-su -c "DISPLAY=:0.0 /bin/bash /userdata/system/pro/sunshine/launcher.sh &" & 
+sleep 12
 
-sleep 5 
+DISPLAY=:0.0 /userdata/system/pro/sunshine/launcher.sh &
+
+sleep 3
 
 if [[ "$(cat /usr/share/applications/sunshine.desktop | grep sunshined.png)" != "" ]]; then 
 cp /userdata/system/pro/sunshine/extras/sunshine.desktop /usr/share/applications/sunshine.desktop 
