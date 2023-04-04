ea2c0a06350cd6dfc61c787c25cfa9b17c9ccd95
uureel <116395185+uureel@users.noreply.github.com>
2023-04-04 18:31:02 +0200

    +

--- lutris/lutris-window.sh
+++ lutris/lutris-window.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 # batocera flatpak lutris fix 
-mkdir /tmp/lutris-fix 2>/dev/null
-wget -q -O /tmp/lutris-fix/lutris-window.sh https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-window.sh
-dos2unix /tmp/lutris-fix/lutris-window.sh 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-window.sh 2>/dev/null
-wget -q -O /tmp/lutris-fix/lutris-rc.xml https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-rc.xml
-dos2unix /tmp/lutris-fix/lutris-rc.xml 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-rc.xml 2>/dev/null
-wget -q -O /tmp/lutris-fix/startx https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-startx
-chmod a+x /tmp/lutris-fix/startx 2>/dev/null
-wget -q -O /tmp/lutris-fix/xinitrc https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-xinitrc
-chmod a+x /tmp/lutris-fix/xinitrc 2>/dev/null
+url=https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris
+f=/tmp/lutris-fix ; mkdir $f 2>/dev/null
+echo
+wget -q -O $f/lutris-window.sh $url/lutris-window.sh
+dos2unix $f/lutris-window.sh 2>/dev/null && chmod a+x $f/lutris-window.sh 2>/dev/null
+echo "> $f/lutris-window.sh"
+wget -q -O $f/lutris-rc.xml $url/lutris-rc.xml
+dos2unix $f/lutris-rc.xml 2>/dev/null && chmod a+x $f/lutris-rc.xml 2>/dev/null
+echo "> $f/lutris-rc.xml"
+wget -q -O $f/startx $url/lutris-startx
+chmod a+x $f/startx 2>/dev/null
+echo "> $f/lutris-startx"
+wget -q -O $f/xinitrc $url/lutris-xinitrc
+chmod a+x $f/xinitrc 2>/dev/null
+echo "> $f/lutris-xinitrc"
+echo
+echo "restarting openbox" 
+sleep 0.1
 cp /tmp/lutrix-fix/lutris-rc.xml /etc/openbox/rc.xml 2>/dev/null
-sleep 0.01
-/tmp/lutris-fix/startx &
+sleep 0.1
+/tmp/lutris-fix/startx 1>/dev/null 2>/dev/null & echo &
