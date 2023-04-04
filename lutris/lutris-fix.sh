9e53d9fc3ba0e0ebc00ef27c3984e1eb4c25e7a2
uureel <116395185+uureel@users.noreply.github.com>
2023-04-04 18:15:43 +0200

    +

--- lutris/lutris-fix.sh
+++ lutris/lutris-fix.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # batocera flatpak lutris fix 
 mkdir /tmp/lutris-fix 2>/dev/null
-wget -q -O /tmp/lutris-fix/lutris-fix.sh https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-window.sh
-dos2unix /tmp/lutris-fix/lutris-fix.sh 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-fix.sh 2>/dev/null
-/tmp/lutris-fix/lutris-fix.sh & 
+wget -q -O /tmp/lutris-fix/lutris-window.sh https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-window.sh
+dos2unix /tmp/lutris-fix/lutris-window.sh 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-window.sh 2>/dev/null
+wget -q -O /tmp/lutris-fix/lutris-rc.xml https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-rc.xml
+dos2unix /tmp/lutris-fix/lutris-rc.xml 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-rc.xml 2>/dev/null
+wget -q -O /tmp/lutris-fix/startx https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-startx
+chmod a+x /tmp/lutris-fix/startx 2>/dev/null
+wget -q -O /tmp/lutris-fix/xinitrc https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-xinitrc
+chmod a+x /tmp/lutris-fix/xinitrc 2>/dev/null
+cp /tmp/lutrix-fix/lutris-rc.xml /etc/openbox/rc.xml 2>/dev/null
+sleep 0.01
+/tmp/lutris-fix/startx &
--- lutris/lutris-window.sh
+++ lutris/lutris-window.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 # batocera flatpak lutris fix 
 mkdir /tmp/lutris-fix 2>/dev/null
-wget -q -O /tmp/lutris-fix/lutris-fix.sh https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-window.sh
-dos2unix /tmp/lutris-fix/lutris-fix.sh 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-fix.sh 2>/dev/null
+wget -q -O /tmp/lutris-fix/lutris-window.sh https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-window.sh
+dos2unix /tmp/lutris-fix/lutris-window.sh 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-window.sh 2>/dev/null
 wget -q -O /tmp/lutris-fix/lutris-rc.xml https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-rc.xml
 dos2unix /tmp/lutris-fix/lutris-rc.xml 2>/dev/null && chmod a+x /tmp/lutris-fix/lutris-rc.xml 2>/dev/null
 wget -q -O /tmp/lutris-fix/startx https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-startx
-chmod a+x /tmp/lutris-fix/lutris-startx 2>/dev/null
+chmod a+x /tmp/lutris-fix/startx 2>/dev/null
 wget -q -O /tmp/lutris-fix/xinitrc https://raw.githubusercontent.com/uureel/batocera.pro/main/lutris/lutris-xinitrc
-chmod a+x /tmp/lutris-fix/lutris-xinitrc 2>/dev/null
-wait 
-sleep 0.01
-if [[ -e /tmp/lutris-fix/lutris-window.sh ]] && [[ `cat /tmp/lutris-fix/lutris-window.sh` > "0" ]] \
-[[ -e /tmp/lutris-fix/lutris-rc.xml ]] && [[ `cat /tmp/lutris-fix/lutris-rc.xml` > "0" ]] \
-[[ -e /tmp/lutris-fix/startx ]] && [[ `cat /tmp/lutris-fix/startx` > "0" ]] \
-[[ -e /tmp/lutris-fix/xinitrc ]] && [[ `cat /tmp/lutris-fix/xinitrc` > "0" ]]; then 
+chmod a+x /tmp/lutris-fix/xinitrc 2>/dev/null
 cp /tmp/lutrix-fix/lutris-rc.xml /etc/openbox/rc.xml 2>/dev/null
 sleep 0.01
-	if [[ -e /bin/su ]]; then 
-		su -c "/tmp/lutris-fix/startx &" root & 
-	else 
-		/tmp/lutris-fix/startx & echo & 
-	fi
-else 
-echo "Missing files? Run the script again.."
-fi
+/tmp/lutris-fix/startx &
