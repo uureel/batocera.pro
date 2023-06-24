598be37b1232b0a15490eb673d8f29f35974d4ef
uureel <116395185+uureel@users.noreply.github.com>
2023-06-24 23:55:19 +0200

    Update launcher.sh

--- ps3plus/extras/launcher.sh
+++ ps3plus/extras/launcher.sh
@@ -18,42 +18,21 @@ rm $log1 2>/dev/null; rm $log2 2>/dev/null
 # prepare *.ai: 
 chmod a+x /userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage
 
-# get scale/resolution: 
-SCALE=1
-res=$(xrandr | grep " connected" | awk '{print $3}' | cut -d "x" -f1)
-if [[ "$res" -le "3840" ]] && [[ "$res" -ge "2560" ]]; then
-SCALE=1.25
-fi 
-if [[ "$res" -lt "2560" ]] && [[ "$res" -ge "1920" ]]; then
-SCALE=1.0
-fi 
-if [[ "$res" -lt "1920" ]] && [[ "$res" -ge "1280" ]]; then
-SCALE=0.75
-fi 
-if [[ "$res" -lt "1280" ]]; then
-SCALE=0.5
-fi 
-
 # start appimage: 
 if [[ "$(echo "$ROM" | grep "CONFIG")" != "" ]] || [[ "$(echo "$ROM")" = "" ]]; then
 unclutter-remote -s; 
-DISPLAY=:0.0 \
-QT_FONT_DPI=128 \
-QT_SCALE_FACTOR=$SCALE \
-XDG_CONFIG_HOME=/userdata/system/configs \
-XDG_CACHE_HOME=/userdata/saves \
-XDG_RUNTIME_DIR=/userdata \
-QT_QPA_PLATFORM=xcb \
-/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage -platform xcb 1>$log1 2>$log2
+	DISPLAY=:0.0 \
+	QT_FONT_DPI=128 \
+	QT_QPA_PLATFORM=xcb \
+	XDG_CONFIG_HOME=/userdata/system/configs \
+		/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage -platform xcb 1>$log1 2>$log2
+#XDG_CACHE_HOME=/userdata/saves \
 else 
-DISPLAY=:0.0 \
-QT_FONT_DPI=128 \
-QT_SCALE_FACTOR=$SCALE \
-XDG_CONFIG_HOME=/userdata/system/configs \
-XDG_CACHE_HOME=/userdata/saves \
-XDG_RUNTIME_DIR=/userdata \
-QT_QPA_PLATFORM=xcb \
-/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage -platform xcb --no-gui "$ROM" 1>$log1 2>$log2
+	DISPLAY=:0.0 \
+	QT_FONT_DPI=128 \
+	QT_QPA_PLATFORM=xcb \
+	XDG_CONFIG_HOME=/userdata/system/configs \
+		/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage -platform xcb --no-gui "$ROM" 1>$log1 2>$log2
 fi
 
 sleep 1
