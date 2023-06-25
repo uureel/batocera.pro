4922c96baf238b231045a163053786ce80b483cb
uureel <116395185+uureel@users.noreply.github.com>
2023-06-26 00:43:20 +0200

    Update dark.sh

--- dark/dark.sh
+++ dark/dark.sh
@@ -2,20 +2,31 @@
 #batocera-darkmode.sh
 #####################
 
-echo
-echo "preparing dark theme for f1/gtk/pcmanfm..."
+echo -e "\n  preparing dark theme for f1/gtk/pcmanfm..."
 
+	# prepare folder 
 	f=/userdata/system/pro/dark
 		mkdir -p $f 2>/dev/null
 
-	url=https://raw.githubusercontent.com/uureel/batocera.pro/main/dark
-		wget -q -O $f/Adwaita-dark.zip $url/Adwaita-dark.zip 
-			unzip -qq $f/Adwaita-dark.zip 
-				yes "A" | cp -r $f/Adwaita-dark /usr/share/themes/
+	# get files 
+	url=https://github.com/uureel/batocera.pro/raw/main/dark/Adwaita-dark.zip
+		if [[ ! -f $f/Adwaita-dark.zip ]]; then 
+		wget -q -O $f/Adwaita-dark.zip "https://github.com/uureel/batocera.pro/raw/main/dark/Adwaita-dark.zip"
+		else 
+			if [[ "$(wc -c "$f/Adwaita-dark.zip" | awk '{print $1}')" < "70000" ]]; then 
+			wget -q -O $f/Adwaita-dark.zip "https://github.com/uureel/batocera.pro/raw/main/dark/Adwaita-dark.zip"
+			fi
+		fi
+			unzip -oq $f/Adwaita-dark.zip 
+				cp -r $f/Adwaita-dark /usr/share/themes/
+		wget -q -O $f/dark.sh "https://github.com/uureel/batocera.pro/raw/main/dark/dark.sh"
+			dos2unix $f/dark.sh 1>/dev/null 2>/dev/null 
+			chmod a+x $f/dark.sh 2>/dev/null
 
+	# add dark theme to f1 launcher 
 	sed -i '/export XDG_CONFIG_DIRS=\/etc\/xdg/a export GTK_THEME=Adwaita-dark' /usr/bin/filemanagerlauncher
 
+	# cookie 
 	export GTK_THEME=Adwaita-dark
 
-echo "done."
-echo
+echo -e "  done 8) \n" 
