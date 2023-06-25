51e37ba70578306aca8a88a569d6d0f8565131be
uureel <116395185+uureel@users.noreply.github.com>
2023-06-25 17:32:42 +0200

    Update launcher.sh

--- ps2plus/extras/launcher.sh
+++ ps2plus/extras/launcher.sh
@@ -4,7 +4,7 @@
 ROM="$1"
 
 # check cpu features: 
-rm /tmp/cpufeatures; cat /proc/cpuinfo | grep flags | head -n 1 | grep avx2 >> /tmp/cpufeatures
+rm /tmp/cpufeatures 2>/dev/null; cat /proc/cpuinfo | grep flags | head -n 1 | grep avx2 >> /tmp/cpufeatures
 
 # prepare logs: 
 log1=/userdata/system/pro/ps2plus/log1.txt
@@ -16,34 +16,36 @@ chmod a+x /userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage 2>/dev/null
 chmod a+x /userdata/system/pro/ps2plus/pcsx2/pcsx2-AVX2.AppImage 2>/dev/null
 chmod a+x /userdata/system/pro/ps2plus/pcsx2/pcsx2-SSE4.AppImage 2>/dev/null
 
-# get scale/resolution: 
-SCALE=1
-res=$(xrandr | grep " connected" | awk '{print $3}' | cut -d "x" -f1)
-if [[ "$res" -le "3840" ]] && [[ "$res" -ge "2560" ]]; then
-SCALE=1.25
-fi 
-if [[ "$res" -lt "2560" ]] && [[ "$res" -ge "1920" ]]; then
-SCALE=1.0
-fi 
-if [[ "$res" -lt "1920" ]] && [[ "$res" -ge "1280" ]]; then
-SCALE=0.75
-fi 
-if [[ "$res" -lt "1280" ]]; then
-SCALE=0.5
-fi 
-
 # start appimage: 
 if [[ "$(cat /tmp/cpufeatures | grep avx2)" != "" ]]; then 
 	if [[ "$(echo "$ROM" | grep "CONFIG")" != "" ]] || [[ "$(echo "$ROM")" = "" ]]; then
-	unclutter-remote -s; DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=$SCALE /userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage 1>$log1 2>$log2
+		unclutter-remote -s;
+			DISPLAY=:0.0 \
+			QT_FONT_DPI=128 \
+			QT_QPA_PLATFORM=xcb \
+			XDG_CONFIG_HOME=/userdata/system/configs \
+				/userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage 1>$log1 2>$log2
 	else 
-	DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=$SCALE /userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage -fullscreen -nogui "$ROM" 1>$log1 2>$log2
+			DISPLAY=:0.0 \
+			QT_FONT_DPI=128 \
+			QT_QPA_PLATFORM=xcb \
+			XDG_CONFIG_HOME=/userdata/system/configs \
+				/userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage -fullscreen -nogui "$ROM" 1>$log1 2>$log2
 	fi
 else 
 	if [[ "$(echo "$ROM" | grep "CONFIG")" != "" ]] || [[ "$(echo "$ROM")" = "" ]]; then
-	unclutter-remote -s; DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=$SCALE /userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage 1>$log1 2>$log2
+		unclutter-remote -s; 
+			DISPLAY=:0.0 \
+			QT_FONT_DPI=128 \
+			QT_QPA_PLATFORM=xcb \
+			XDG_CONFIG_HOME=/userdata/system/configs \
+				/userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage 1>$log1 2>$log2
 	else 
-	DISPLAY=:0.0 QT_FONT_DPI=128 QT_SCALE_FACTOR=$SCALE /userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage -fullscreen -nogui "$ROM" 1>$log1 2>$log2
+			DISPLAY=:0.0 \
+			QT_FONT_DPI=128 \
+			QT_QPA_PLATFORM=xcb \
+			XDG_CONFIG_HOME=/userdata/system/configs \
+				/userdata/system/pro/ps2plus/pcsx2/pcsx2.AppImage -fullscreen -nogui "$ROM" 1>$log1 2>$log2
 	fi 
 fi
 
