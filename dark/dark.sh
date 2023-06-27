17591b00ba043defb4924e7eb071b3458ca1700e
uureel <116395185+uureel@users.noreply.github.com>
2023-06-27 14:47:50 +0200

    +chiaki_latest@270623

--- dark/dark.sh
+++ dark/dark.sh
@@ -2,19 +2,20 @@
 #batocera-darkmode.sh
 #####################
 
-echo "preparing dark theme for f1/gtk/pcmanfm..."
 echo
+echo "preparing dark theme for f1/gtk/pcmanfm..."
 
-f=/userdata/system/pro/dark
-	mkdir -p $f 2>/dev/null
+	f=/userdata/system/pro/dark
+		mkdir -p $f 2>/dev/null
 
-url=https://raw.githubusercontent.com/uureel/batocera.pro/main/dark
-	wget -q -O $f/Adwaita-dark.zip $url/Adwaita-dark.zip 
-		unzip -qq $f/Adwaita-dark.zip 
-			mv $f/Adwaita-dark /usr/share/themes/
+	url=https://raw.githubusercontent.com/uureel/batocera.pro/main/dark
+		wget -q -O $f/Adwaita-dark.zip $url/Adwaita-dark.zip 
+			yes | unzip -oq $f/Adwaita-dark.zip 
+				cp -r $f/Adwaita-dark /usr/share/themes/
 
-sed -i '/export XDG_CONFIG_DIRS=\/etc\/xdg/a export GTK_THEME=Adwaita-dark' /usr/bin/filemanagerlauncher
+	sed -i '/export XDG_CONFIG_DIRS=\/etc\/xdg/a export GTK_THEME=Adwaita-dark' /usr/bin/filemanagerlauncher
 
-export GTK_THEME=Adwaita-dark
+	export GTK_THEME=Adwaita-dark
 
 echo "done."
+echo
