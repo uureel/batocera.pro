3106394fed7610f125f17bf0b4615690b655baa8
uureel <116395185+uureel@users.noreply.github.com>
2023-10-02 14:36:12 +0200

    Delete bedrock directory

--- bedrock/extras/UNINSTALL.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /userdata/system/
-
-rm -rf /userdata/system/pro/bedrock 2>/dev/null 
-rm /usr/share/bedrock.desktop 2>/dev/null 
-
-curl http://127.0.0.1:1234/reloadgames 
--- bedrock/extras/startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-cp /userdata/system/pro/bedrock/extras/bedrock.desktop /usr/share/applications/ 2>/dev/null
