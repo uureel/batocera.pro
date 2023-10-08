a15a623408e3fb5495cff9657898c81e45ef110f
uureel <116395185+uureel@users.noreply.github.com>
2023-10-08 19:20:24 +0200

    Update startup.sh

--- sunshine/extras/startup.sh
+++ sunshine/extras/startup.sh
@@ -8,11 +8,11 @@ rm $log1 2>/dev/null ; rm $log2 2>/dev/null
 
 cp /userdata/system/pro/sunshine/extras/sunshine.desktop /usr/share/applications/ 2>/dev/null 
 
-sleep 22
+sleep 44
 
 su -c "nohup /userdata/system/pro/sunshine/launcher.sh &" root &
 
-sleep 2
+sleep 1
 
 if [[ "$(cat /usr/share/applications/sunshine.desktop | grep sunshined.png)" != "" ]]; then 
 cp /userdata/system/pro/sunshine/extras/sunshine.desktop /usr/share/applications/sunshine.desktop 
