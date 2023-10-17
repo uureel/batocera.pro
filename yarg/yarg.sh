c4dad59c64376f522a05441b5a05f99d33f618a6
uureel <116395185+uureel@users.noreply.github.com>
2023-10-17 20:04:36 +0200

    +

---
+++ yarg/yarg.sh
@@ -0,0 +1,402 @@
+#!/usr/bin/env bash 
+######################################################################
+# BATOCERA.PRO INSTALLER
+######################################################################
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+APPNAME=YARG # for installer info
+appname=yarg # directory name in /userdata/system/pro/...
+AppName=yarg # App.AppImage name
+APPPATH=/userdata/system/pro/$appname/batocera-$appname.AppImage
+APPLINK=http://batocera.pro/app/batocera-yarg.AppImage
+ORIGIN=batocera.pro@github.com/YARC-Official/YARG # credit & info
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# show console/ssh info: 
+clear
+echo
+echo
+echo
+echo -e "${X}PREPARING $APPNAME INSTALLER, PLEASE WAIT . . . ${X}"
+echo
+echo
+echo
+echo
+# --------------------------------------------------------------------
+# -- output colors:
+###########################
+W='\033[0;37m'            # white
+#-------------------------#
+RED='\033[1;31m'          # red
+BLUE='\033[1;34m'         # blue
+GREEN='\033[1;32m'        # green
+PURPLE='\033[1;35m'       # purple
+DARKRED='\033[0;31m'      # darkred
+DARKBLUE='\033[0;34m'     # darkblue
+DARKGREEN='\033[0;32m'    # darkgreen
+DARKPURPLE='\033[0;35m'   # darkpurple
+###########################
+# console theme
+X='\033[0m' # / resetcolor
+L=$X
+R=$X
+# --------------------------------------------------------------------
+# prepare paths and files for installation 
+# paths:
+cd ~/
+pro=/userdata/system/pro
+mkdir $pro 2>/dev/null
+mkdir $pro/extra 2>/dev/null
+mkdir $pro/$appname 2>/dev/null
+mkdir $pro/$appname/extra 2>/dev/null
+# --------------------------------------------------------------------
+# -- prepare dependencies for this app and the installer: 
+url=https://github.com/uureel/batocera.pro/raw/main/.dep
+depfile=dependencies.txt; dep=$pro/.dep; mkdir $pro/.dep 2>/dev/null; cd $dep
+wget -q -O $dep/$depfile $url/$depfile 2>/dev/null; dos2unix $dep/$depfile 1>/dev/null 2>/dev/null;
+rm /userdata/system/pro/.dep/libtinfo.so.6 2>/dev/null
+nl=$(cat $dep/$depfile | wc -l); l=1; while [[ "$l" -le "$((nl+2))" ]]; do
+d=$(cat $dep/$depfile | sed ""$l"q;d"); wget -q -O $dep/$d $url/$d 2>/dev/null; 
+if [[ "$(echo $d | grep "lib")" != "" ]]; then ln -s $dep/$d /lib/$d 2>/dev/null; fi; ((l++)); done
+wget -q -O $pro/$appname/extra/icon.png https://github.com/uureel/batocera.pro/raw/main/$appname/extra/icon.png; chmod a+x $dep/tput; cd ~/
+# --------------------------------------------------------------------
+# // end of dependencies 
+#
+# RUN BEFORE INSTALLER: 
+######################################################################
+killall wget 2>/dev/null && killall $AppName 2>/dev/null && killall $AppName 2>/dev/null && killall $AppName 2>/dev/null
+######################################################################
+# 
+# --------------------------------------------------------------------
+# show console/ssh info: 
+clear
+echo
+echo
+echo
+echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo
+echo
+echo
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+# --------------------------------------------------------------------
+clear
+echo
+echo
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo -e "${X}--------------------------------------------------------"
+echo
+echo
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+# --------------------------------------------------------------------
+clear
+echo
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}--------------------------------------------------------"
+echo
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+# --------------------------------------------------------------------
+clear
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}BATOCERA.PRO/$APPNAME INSTALLER${X}"
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}--------------------------------------------------------"
+echo -e "${X}--------------------------------------------------------"
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+echo -e "${X}THIS WILL INSTALL $APPNAME FOR BATOCERA"
+echo -e "${X}USING $ORIGIN"
+echo
+echo -e "${X}$APPNAME WILL BE AVAILABLE IN F1->APPLICATIONS "
+echo -e "${X}AND INSTALLED IN /USERDATA/SYSTEM/PRO/$APPNAME"
+echo
+echo -e "${X}. . .${X}" 
+echo
+# // end of console info. 
+#
+#/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+#\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+#
+# THIS WILL BE SHOWN ON MAIN BATOCERA DISPLAY:   
+function batocera-pro-installer {
+APPNAME="$1"
+appname="$2"
+AppName="$3"
+APPPATH="$4"
+APPLINK="$5"
+ORIGIN="$6"
+# -- colors: 
+###########################
+X='\033[0m'               # 
+W='\033[0m'               # 
+#-------------------------#
+RED='\033[0m'             # 
+BLUE='\033[0m'            # 
+GREEN='\033[0m'           # 
+PURPLE='\033[0m'          # 
+DARKRED='\033[0m'         # 
+DARKBLUE='\033[0m'        # 
+DARKGREEN='\033[0m'       # 
+DARKPURPLE='\033[0m'      # 
+###########################
+# -- display theme:
+L=$W
+T=$W
+R=$RED
+B=$BLUE
+G=$GREEN
+P=$PURPLE
+# .
+######################################################################
+clear
+echo
+echo
+echo
+echo -e "${W}BATOCERA.PRO/${G}$APPNAME${W} INSTALLER ${W}"
+echo
+echo
+echo
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+# --------------------------------------------------------------------
+clear
+echo
+echo
+echo
+echo -e "${W}BATOCERA.PRO/${W}$APPNAME${W} INSTALLER ${W}"
+echo
+echo
+echo
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+# --------------------------------------------------------------------
+clear
+echo
+echo
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${W}BATOCERA.PRO/${G}$APPNAME${W} INSTALLER ${W}"
+echo -e "${L}-----------------------------------------------------------------------"
+echo
+echo
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+# --------------------------------------------------------------------
+clear
+echo
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${W}BATOCERA.PRO/${W}$APPNAME${W} INSTALLER ${W}"
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${L}-----------------------------------------------------------------------"
+echo
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+# --------------------------------------------------------------------
+clear
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${W}BATOCERA.PRO/${G}$APPNAME${W} INSTALLER ${W}"
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${L}-----------------------------------------------------------------------"
+echo
+# --------------------------------------------------------------------
+sleep 0.33
+echo -e "${W}THIS WILL INSTALL $APPNAME FOR BATOCERA"
+echo -e "${W}USING $ORIGIN"
+echo
+echo -e "${W}$APPNAME WILL BE AVAILABLE IN F1->APPLICATIONS "
+echo -e "${W}AND INSTALLED IN /USERDATA/SYSTEM/PRO/$APPNAME"
+echo
+echo -e "${L}- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+# -- check system before proceeding
+if [[ "$(uname -a | grep "x86_64")" != "" ]]; then 
+:
+else
+echo
+echo -e "${RED}ERROR: SYSTEM NOT SUPPORTED"
+echo -e "${RED}YOU NEED BATOCERA X86_64${X}"
+echo
+sleep 5
+exit 0
+# quit
+exit 0
+fi
+#
+# -- temp for curl download
+temp=/userdata/system/pro/$appname/extra/downloads
+rm -rf $temp 2>/dev/null
+mkdir $temp 2>/dev/null
+#
+# --------------------------------------------------------------------
+#
+echo
+echo -e "${G}DOWNLOADING${W} $APPNAME . . ."
+sleep 1
+echo -e "${T}$APPLINK" | sed 's,https://,> ,g' | sed 's,http://,> ,g' 2>/dev/null
+cd $temp
+curl --progress-bar --remote-name --location "$APPLINK"
+cd ~/
+mv $temp/* $APPPATH 2>/dev/null
+chmod a+x $APPPATH 2>/dev/null
+rm -rf $temp/*.AppImage
+SIZE=$(($(wc -c $APPPATH | awk '{print $1}')/1048576)) 2>/dev/null
+echo -e "${T}$APPPATH ${T}$SIZE( )MB ${G}OK${W}" | sed 's/( )//g'
+echo -e "${G}> ${W}DONE"
+echo
+echo -e "${L}- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+sleep 1.333
+#
+# --------------------------------------------------------------------
+#
+echo
+echo -e "${G}INSTALLING ${W}. . ."
+# -- get icon for shortcut,
+icon=/userdata/system/pro/$appname/extra/icon.png
+mkdir -p /userdata/system/pro/$appname/extra 2>/dev/null
+wget -q -O $icon https://github.com/uureel/batocera.pro/raw/main/$appname/extra/icon.png
+# //
+# -- prepare yarg launcher, 
+launcher=/userdata/system/pro/$appname/yarg
+rm -rf $launcher
+echo '#!/bin/bash ' >> $launcher
+echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg.AppImage start "$@"' >> $launcher
+dos2unix $launcher 2>/dev/null
+chmod a+x $launcher 2>/dev/null
+# //
+# -- prepare yarc launcher, 
+launcher=/userdata/system/pro/$appname/yarc
+rm -rf $launcher
+echo '#!/bin/bash ' >> $launcher
+echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg.AppImage launcher "$@"' >> $launcher
+dos2unix $launcher 2>/dev/null
+chmod a+x $launcher 2>/dev/null
+# //
+# -- prepare f1 - applications - YARG shortcut, 
+shortcut=/userdata/system/pro/$appname/extra/YARG.desktop
+rm -rf $shortcut 2>/dev/null
+echo "[Desktop Entry]" >> $shortcut
+echo "Version=1.0" >> $shortcut
+echo "Icon=/userdata/system/pro/$appname/extra/icon.png" >> $shortcut
+echo "Exec=/userdata/system/pro/$appname/yarg %F" >> $shortcut
+echo "Terminal=false" >> $shortcut
+echo "Type=Application" >> $shortcut
+echo "Categories=Game;batocera.linux;" >> $shortcut
+echo "Name=YARG" >> $shortcut
+f1shortcut=/usr/share/applications/YARG.desktop
+cp $shortcut $f1shortcut 2>/dev/null
+# //
+# -- prepare f1 - applications - YARC Launcher shortcut, 
+shortcut=/userdata/system/pro/$appname/extra/YARC.desktop
+rm -rf $shortcut 2>/dev/null
+echo "[Desktop Entry]" >> $shortcut
+echo "Version=1.0" >> $shortcut
+echo "Icon=/userdata/system/pro/$appname/extra/icon.png" >> $shortcut
+echo "Exec=/userdata/system/pro/$appname/yarc %F" >> $shortcut
+echo "Terminal=false" >> $shortcut
+echo "Type=Application" >> $shortcut
+echo "Categories=Game;batocera.linux;" >> $shortcut
+echo "Name=YARC-launcher" >> $shortcut
+f1shortcut=/usr/share/applications/YARC.desktop
+cp $shortcut $f1shortcut 2>/dev/null
+# //
+# -- prepare ports - YARG shortcut, 
+shortcut=/userdata/roms/ports/YARG.sh
+rm -rf $shortcut 2>/dev/null
+echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg start "${@}"' >> $shortcut
+chmod 777 $shortcut 2>/dev/null
+# -- prepare ports - YARC Launcher shortcut, 
+shortcut="/userdata/roms/ports/YARC-Launcher.sh"
+rm -rf $shortcut 2>/dev/null
+echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg launcher "${@}"' >> $shortcut
+chmod 777 $shortcut 2>/dev/null
+# //
+#
+# -- prepare prelauncher to avoid overlay,
+pre=/userdata/system/pro/$appname/extra/startup
+rm -rf $pre 2>/dev/null
+echo "#!/usr/bin/env bash" >> $pre
+echo "cp /userdata/system/pro/$appname/extra/YARG.desktop /usr/share/applications/ 2>/dev/null" >> $pre
+echo "cp /userdata/system/pro/$appname/extra/YARC.desktop /usr/share/applications/ 2>/dev/null" >> $pre
+dos2unix $pre
+chmod a+x $pre
+# // 
+# 
+# -- add prelauncher to custom.sh to run @ reboot
+customsh=/userdata/system/custom.sh
+if [[ -e $customsh ]] && [[ "$(cat $customsh | grep "/userdata/system/pro/$appname/extra/startup")" = "" ]]; then
+echo -e "\n/userdata/system/pro/$appname/extra/startup" >> $customsh
+fi
+if [[ -e $customsh ]] && [[ "$(cat $customsh | grep "/userdata/system/pro/$appname/extra/startup" | grep "#")" != "" ]]; then
+echo -e "\n/userdata/system/pro/$appname/extra/startup" >> $customsh
+fi
+if [[ -e $customsh ]]; then :; else
+echo -e "\n/userdata/system/pro/$appname/extra/startup" >> $customsh
+fi
+dos2unix $customsh 2>/dev/null
+# //
+#
+# -- done. 
+sleep 1
+echo -e "${G}> ${W}DONE"
+echo
+sleep 1
+echo -e "${L}-----------------------------------------------------------------------"
+echo -e "${W}> $APPNAME INSTALLED ${G}OK"
+echo -e "${L}-----------------------------------------------------------------------"
+sleep 4
+}
+export -f batocera-pro-installer 2>/dev/null
+# --------------------------------------------------------------------
+# RUN:
+  batocera-pro-installer "$APPNAME" "$appname" "$AppName" "$APPPATH" "$APPLINK" "$ORIGIN" 2>/dev/null
+# --------------------------------------------------------------------
+function autostart() {
+  csh="/userdata/system/custom.sh"
+  pcsh="/userdata/system/pro-custom.sh"
+  pro="/userdata/system/pro"
+  rm -f $pcsh
+  temp_file=$(mktemp)
+  find $pro -type f \( -path "*/extra/startup" -o -path "*/extras/startup.sh" \) > $temp_file
+  echo "#!/bin/bash" > $pcsh
+  sort $temp_file >> $pcsh
+  rm $temp_file
+  chmod a+x $pcsh
+  temp_csh=$(mktemp)
+  grep -vxFf $pcsh $csh > $temp_csh
+  mapfile -t lines < $temp_csh
+  if [[ "${lines[0]}" != "#!/bin/bash" ]]; then
+    lines=( "#!/bin/bash" "${lines[@]}" )
+  fi
+  if ! grep -Fxq "$pcsh &" $temp_csh; then
+    lines=( "${lines[0]}" "$pcsh &" "${lines[@]:1}" )
+  fi
+  printf "%s\n" "${lines[@]}" > $csh
+  chmod a+x $csh
+  rm $temp_csh
+}
+export -f autostart
+autostart
+exit 0
+# BATOCERA.PRO INSTALLER //
+##########################
