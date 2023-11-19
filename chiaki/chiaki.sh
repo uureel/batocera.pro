7027aac78947a1c769447f4b567ca8f51d7e7fb4
uureel <116395185+uureel@users.noreply.github.com>
2023-11-19 21:31:43 +0100

    +

--- chiaki/chiaki.sh
+++ chiaki/chiaki.sh
@@ -17,9 +17,9 @@
 # DEFINE APP INFO >>
 APPNAME="chiaki"; appname=$(echo "$APPNAME" | awk '{print tolower($0)}'); 
 #APPLINK=https://git.sr.ht/~thestr4ng3r/chiaki/refs/download/v2.1.1/Chiaki-v2.1.1-Linux-x86_64.AppImage
-#updating appimage to include latest source @ 27062023 
-APPLINK=https://github.com/uureel/batocera.pro/raw/main/chiaki/extra/chiaki.AppImage
-APPHOME="git.sr.ht/~thestr4ng3r/chiaki v2.1.1@27062023"
+#APPLINK=https://github.com/uureel/batocera.pro/raw/main/chiaki/extra/chiaki.AppImage
+APPLINK=https://git.sr.ht/~thestr4ng3r/chiaki/refs/download/v2.2.0/Chiaki-v2.2.0-Linux-x86_64.AppImage
+APPHOME="git.sr.ht/~thestr4ng3r/chiaki v2.2.0"
 #---------------------------------------------------------------------
 # DEFINE LAUNCHER COMMAND >>
 COMMAND='mkdir /userdata/system/pro/'$appname'/home 2>/dev/null; mkdir /userdata/system/pro/'$appname'/config 2>/dev/null; mkdir /userdata/system/pro/'$appname'/roms 2>/dev/null; LD_LIBRARY_PATH="/userdata/system/pro/.dep:${LD_LIBRARY_PATH}" HOME=/userdata/system/pro/'$appname'/home XDG_CONFIG_HOME=/userdata/system/pro/'$appname'/config QT_SCALE_FACTOR="1" GDK_SCALE="1" XDG_DATA_HOME=/userdata/system/pro/'$appname'/home DISPLAY=:0.0 /userdata/system/pro/'$appname'/'$appname'.AppImage'
