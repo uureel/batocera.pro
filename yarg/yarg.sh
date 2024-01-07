9ff0f80efaac75f0043ccb3b53658d403b5b876d
giwty <55044210+giwty@users.noreply.github.com>
2024-01-06 22:23:47 -0800

    Fix YARG not starting up
    At least in my case it was not starting because of that

--- yarg/yarg.sh
+++ yarg/yarg.sh
@@ -325,7 +325,7 @@ shortcut=/userdata/roms/ports/YARG.sh
 rm -rf $shortcut 2>/dev/null
 echo 'export DISPLAY=:0.0' >> $shortcut
 echo '#unclutter-remote -s' >> $shortcut
-echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg start "${@}"' >> $shortcut
+echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg.AppImage start "${@}"' >> $shortcut
 echo 'unclutter-remote -h' >> $shortcut
 chmod 777 $shortcut 2>/dev/null
 # -- prepare ports - YARC Launcher shortcut, 
@@ -333,7 +333,7 @@ shortcut="/userdata/roms/ports/YARC-Launcher.sh"
 rm -rf $shortcut 2>/dev/null
 echo 'export DISPLAY=:0.0' >> $shortcut
 echo 'unclutter-remote -s' >> $shortcut
-echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg launcher "${@}"' >> $shortcut
+echo 'DISPLAY=:0.0 /userdata/system/pro/yarg/batocera-yarg.AppImage launcher "${@}"' >> $shortcut
 echo 'unclutter-remote -h' >> $shortcut
 chmod 777 $shortcut 2>/dev/null
 # //
