f2a21095afcb89c16c7ddda74cc4e955996c5d45
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-09 14:25:36 +0200

    Update install.sh

--- app/install.sh
+++ app/install.sh
@@ -1,1 +1,49 @@
+#!/bin/bash
+# Function to display animated title
+animate_title() {
+    local text="BATOCERA PRO APP INSTALLER"
+    local delay=0.1
+    local length=${#text}
 
+    for (( i=0; i<length; i++ )); do
+        echo -n "${text:i:1}"
+        sleep $delay
+    done
+    echo
+}
+
+# Function to display controls
+display_controls() {
+    echo "Controls:"
+    echo "  This Will install Batocera Pro app Installer to Ports"
+    echo    
+    sleep 5  # Delay for 5 seconds
+}
+
+# Main script execution
+clear
+animate_title
+display_controls
+
+
+
+
+# Check if /userdata/system/pro does not exist and create it if necessary
+if [ ! -d "/userdata/system/pro" ]; then
+    mkdir -p /userdata/system/pro
+fi
+
+# Download pro.sh to /userdata/system/pro
+curl -L https://github.com/uureel/batocera.pro/raw/main/app/pro.sh -o /userdata/system/pro/pro.sh
+
+# Download BatoceraPRO.sh to /userdata/roms/ports
+curl -L https://github.com/uureel/batocera.pro/raw/main/app/BatoceraPRO.sh -o /userdata/roms/ports/BatoceraPRO.sh
+
+# Download BatoceraPRO.sh.keys to /userdata/roms/ports
+curl -L https://github.com/uureel/batocera.pro/raw/main/raww/BatoceraPRO.sh.keys -o /userdata/roms/ports/BatoceraPRO.sh.keys
+
+# Set execute permissions for the downloaded scripts
+chmod +x /userdata/system/pro/pro.sh
+chmod +x /userdata/roms/ports/BatoceraPRO.sh
+
+killall -9 emulationstation
