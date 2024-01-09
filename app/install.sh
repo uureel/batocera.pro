879152836310986f43cc907adfc221750bc72501
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-09 15:21:41 +0200

    Update install.sh

--- app/install.sh
+++ app/install.sh
@@ -40,10 +40,12 @@ curl -L https://github.com/uureel/batocera.pro/raw/main/app/pro.sh -o /userdata/
 curl -L https://github.com/uureel/batocera.pro/raw/main/app/BatoceraPRO.sh -o /userdata/roms/ports/BatoceraPRO.sh
 
 # Download BatoceraPRO.sh.keys to /userdata/roms/ports
-curl -L https://github.com/uureel/batocera.pro/raw/main/raww/BatoceraPRO.sh.keys -o /userdata/roms/ports/BatoceraPRO.sh.keys
+curl -L https://github.com/uureel/batocera.pro/raw/main/raw/BatoceraPRO.sh.keys -o /userdata/roms/ports/BatoceraPRO.sh.keys
 
 # Set execute permissions for the downloaded scripts
 chmod +x /userdata/system/pro/pro.sh
 chmod +x /userdata/roms/ports/BatoceraPRO.sh
 
 killall -9 emulationstation
+
+echo "Finished"
