2327ae3191532523c25924bcd1d0f52c4e94c0c0
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-23 18:04:05 +0200

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -50,7 +50,7 @@ DOWNLOAD_URL="batocera.pro/app/conty.sh"
 DOWNLOAD_FILE="$BASE_DIR/conty.sh"
 ROMS_DIR="/userdata/roms/ports"
 
-cvlc https://github.com/trashbus99/batocera-addon-scripts/raw/main/media/wait.mp3 --repeat &
+cvlc https://github.com/trashbus99/batocera-addon-scripts/raw/main/media/wait.mp3 --repeat >/dev/null 2>&1 &
 sleep 10
 
 # Step 1: Create base folder if not exists
