7fb91fea4ef5540e8320e907d7979448dca0d7ff
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-24 18:19:33 +0200

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -9,7 +9,9 @@ if [ "$architecture" != "x86_64" ]; then
     exit 1
 fi
 
-cvlc https://github.com/trashbus99/batocera-addon-scripts/raw/main/media/steam.mp4
+echo..Preparing Steam Installer....
+
+cvlc https://github.com/trashbus99/batocera-addon-scripts/raw/main/media/steam.mp4 >/dev/null 2>&1 &
 
 # Function to display animated title
 animate_title() {
