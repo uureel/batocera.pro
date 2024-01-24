6fd0159e9f65c9252ba403babe651f2dadec153a
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-24 22:48:25 +0200

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -177,8 +177,8 @@ echo "Steam is now starting"
 /userdata/roms/conty/"Steam Big Picture Mode.sh"
 
 
-
+clear
 
 echo "Install Done.  You should see a new system called Linux or Arch Container depending on theme"
-sleep 3
+sleep 5
 killall -9 emulationstation
