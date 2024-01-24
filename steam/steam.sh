3f31608db299bdbc54b86a9b411a270568fa2346
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-24 18:23:33 +0200

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -11,7 +11,9 @@ fi
 
 echo "Batocera PRO - Preparing Steam Installer"
 
-cvlc https://github.com/trashbus99/batocera-addon-scripts/raw/main/media/steam.mp4 >/dev/null 2>&1 
+cvlc https://github.com/trashbus99/batocera-addon-scripts/raw/main/media/steam.mp4 >/dev/null 
+
+killall -9 vlc
 
 Clear 
 
