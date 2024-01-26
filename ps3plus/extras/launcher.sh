dfa4229db28997cbcc74790bbd1aef38ded7ef5e
uureel <116395185+uureel@users.noreply.github.com>
2024-01-26 18:58:39 +0100

    +

--- ps3plus/extras/launcher.sh
+++ ps3plus/extras/launcher.sh
@@ -11,8 +11,8 @@
 		fi
 
 # prepare logs: 
-	log1=/userdata/system/pro/ps3plus/log1.txt
-	log2=/userdata/system/pro/ps3plus/log2.txt
+	log1=/userdata/system/logs/ps3plus-out.txt
+	log2=/userdata/system/logs/ps3plus-err.txt
 		rm $log1 2>/dev/null
 		rm $log2 2>/dev/null
 
@@ -43,7 +43,7 @@ if [[ "$(echo "$ROM" | grep "CONFIG")" != "" ]] || [[ "$(echo "$ROM")" = "" ]]; 
 		QT_QPA_PLATFORM=xcb \
 		AMD_VULKAN_ICD=RADV \
 		DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1 \
-			/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage 
+			/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage > >(tee "$log1") 2> >(tee "$log2" >&2)
 else 
 		DISPLAY=:0.0 \
 		QT_FONT_DPI=128 \
@@ -52,7 +52,7 @@ else
 		QT_QPA_PLATFORM=xcb \
 		AMD_VULKAN_ICD=RADV \
 		DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1 \
-			/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage --no-gui "$ROM"
+			/userdata/system/pro/ps3plus/rpcs3/rpcs3.AppImage --no-gui "$ROM" > >(tee "$log1") 2> >(tee "$log2" >&2)
 fi
 
 
