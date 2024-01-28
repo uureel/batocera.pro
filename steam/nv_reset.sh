872d86eacf71298a444afbc049b145694b099620
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-28 12:44:31 +0200

    Update nv_reset.sh

--- steam/nv_reset.sh
+++ steam/nv_reset.sh
@@ -11,7 +11,7 @@ if dialog --title "Confirm" --yesno "Do you want to proceed and delete the folde
     echo ""
     echo "" 
     echo "Attempting to start Steam.."
-    /userdata/roms/"Steam Big Picture Mode.sh"
+    /userdata/roms/conty/"Steam Big Picture Mode.sh"
 else
     echo "Operation canceled. No folders were deleted."
 fi
