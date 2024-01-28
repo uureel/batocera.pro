851e63e10653a6a258af26ae32b8e0791bbeb2c9
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-28 21:41:27 +0200

    Delete steam/shortcuts/br.sh

--- steam/shortcuts/br.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.local
-chmod 777 ~/.local
-chmod 777 ~/.local/*
-
-
-unclutter-remote -s
-
-ALLOW_ROOT=1  HOME_DIR="/userdata/system/pro/steam/home" DISPLAY=:0.0 ~/pro/steam/conty.sh  env QT_QPA_PLATFORM=xcb mcpelauncher --appimage-extract-and-run
