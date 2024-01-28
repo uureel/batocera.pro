303a18a9aca89aa9820101f61f1378eb4fd5a8a6
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-28 11:26:29 +0200

    Update and rename nv_reset.sh to uninstall.sh

---
+++ steam/uninstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Introductory message using dialog
+dialog --title "Warning" --msgbox "This script will uninstall the arch container and delete data in the following:\n\n1. ~/.local/share/Conty /\n2. ~/pro/steam \n\nThere may be saves.  You can back those up first if you desire." 15 60
+
+# Confirmation dialog
+if dialog --title "Confirm" --yesno "Do you want to proceed the uninstall and delete the folders? Select 'yes' to continue." 10 60; then
+    rm -rf ~/pro/steam   
+    rm -rf ~/.local/share/Conty
+    rm -rf ~/userdata/roms/conty
+    rm -rf ~/userdata/roms/steam2
+    rm ~/configs/emulationstation/es_systems_arch.cfg*
+    rm ~/configs/emulationstation/es_systems_steam2.cfg*
+    echo "Files/Folders deleted."
+    echo ""
+    echo "" 
+    Wait 5
+    killall -9 emulationstation
+else
+    echo "Operation canceled. No folders were deleted."
+fi
