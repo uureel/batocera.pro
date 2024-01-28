bfca5f95e3c4dc94f9df12ba89016f4260b069f4
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-28 10:53:52 +0200

    Update nv_reset.sh

--- steam/nv_reset.sh
+++ steam/nv_reset.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "WARNING: This script will delete data in the following folders to reset nvidia container driver install"
-echo "1. ~/.local"
-echo "2. ~/pro/steam/home"
-echo "There may be saves from other AppImages, apps in ~/.local You can back those up first if you desire"
-echo ""
+# Introductory message using dialog
+dialog --title "Warning" --msgbox "This script will delete data in the following folders to reset the Nvidia container driver install:\n\n1. ~/.local\n2. ~/pro/steam/home\n\nThere may be saves from other AppImages, apps in ~/.local. You can back those up first if you desire." 15 60
 
-# Using dialog to confirm user's action
+# Confirmation dialog
 if dialog --title "Confirm" --yesno "Do you want to proceed and delete the folders? Type 'yes' to continue." 10 60; then
     rm -rf ~/pro/steam/home
     rm -rf ~/.local    
