779ade67187f73b4228279b111597b7471795dcb
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-31 18:48:35 +0200

    Update bedrock.sh

--- bedrock/bedrock.sh
+++ bedrock/bedrock.sh
@@ -7,25 +7,14 @@ dialog --title "WARNING!" \
                 \n2. This game requires you to own Minecraft Bedrock Edition for Android on your Google account.\
                 \n\nCONTINUE INSTALLING?" 15 70
 
-# Save the exit status
+# Capture the exit status of dialog command
 response=$?
 
-# Clear the dialog
-clear
-
-# Act based on the exit status
+# Check the response
 if [ $response -eq 0 ]; then
-    echo "Agreed. Downloading and executing script..."
-    curl -L -o br.sh https://raw.githubusercontent.com/uureel/batocera.pro/main/bedrock/br.sh
-    if [ $? -eq 0 ]; then
-        bash br.sh || echo "Failed to execute the script."
-    else
-        echo "Failed to download the script."
-    fi
-elif [ $response -eq 1 ]; then
-    echo "Declined. Exiting..."
-    exit 0
+    # User selected Yes, execute the curl command
+    curl -L https://raw.githubusercontent.com/uureel/batocera.pro/main/bedrock/br.sh | bash
 else
-    echo "Exited unexpectedly with status $response"
-    exit $response
+    # User selected No, exit the script
+    exit
 fi
