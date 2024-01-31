0095388d188b096b4c35e56ef2edafb285073032
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-01-31 18:27:42 +0200

    Update bedrock.sh

--- bedrock/bedrock.sh
+++ bedrock/bedrock.sh
@@ -1,24 +1,24 @@
-
 #!/bin/bash
 
 # Define the dialog box with increased dimensions
 dialog --title "WARNING!" \
-       --yesno "1. This game is unstable in Batocera. Using the Keyboard in the game world causes it to crash.\
-                \n  Limited testing using only the gamepad seems to work OK. The Keyboard works ok for menus.\
-                \n2.This game requires you own  Minecraft Bedrock Edition for Android on your google account.
-                \n CONTINUE INSTALLING?" 15 70
-
-# Check the exit status
-response=$?
-case $response in
-   0) echo "Agreed. Executing curl command...";
-      # Replace the following line with your actual curl command
-      curl https://github.com/uureel/batocera.pro/raw/main/bedrock/br.sh;;
-   1) echo "Declined.";;
-   255) echo "[ESC] key pressed.";;
-esac
+       --yesno "1. This game is unstable in Batocera. Using the Keyboard in the game world may cause crashes.\
+                \n  Limited testing with a gamepad seems to work. The Keyboard functions for menus.\
+                \n2. This game requires you to own Minecraft Bedrock Edition for Android on your Google account.\
+                \n\nCONTINUE INSTALLING?" 15 70
 
 # Clear the dialog
 clear
 
-
+# Check the exit status
+response=$?
+case $response in
+   0) echo "Agreed. Executing curl command..."
+      # Execute the curl command and handle any errors
+      curl https://github.com/uureel/batocera.pro/raw/main/bedrock/br.sh || echo "Failed to execute curl command."
+      ;;
+   1) echo "Declined."
+      ;;
+   255) echo "[ESC] key pressed."
+      ;;
+esac
