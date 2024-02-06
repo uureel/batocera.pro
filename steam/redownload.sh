47a75abef951bb27e57e9b100cd1d9d1222be0de
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-06 14:51:59 +0200

    Create redownload.sh

---
+++ steam/redownload.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Get the machine hardware name
+architecture=$(uname -m)
+
+# Check if the architecture is x86_64 (AMD/Intel)
+if [ "$architecture" != "x86_64" ]; then
+    echo "This script only runs on AMD or Intel (x86_64) CPUs, not on $architecture."
+    exit 1
+fi
+
+
+Clear 
+
+# Function to display animated title
+animate_title() {
+    local text="Downloading Conty Container"
+    local delay=0.1
+    local length=${#text}
+
+    for (( i=0; i<length; i++ )); do
+        echo -n "${text:i:1}"
+        sleep $delay
+    done
+    echo
+}
+
+
+
+# Main script execution
+clear
+animate_title
+display_controls
+
+cvlc https://github.com/trashbus99/batocera-addon-scripts/raw/main/media/wait.mp3 --repeat >/dev/null 2>&1 &
+
+
+# Step 1: Create base folder if not exists
+mkdir -p "$BASE_DIR"
+if [ ! -d "$BASE_DIR" ]; then
+  # Handle error or exit if necessary
+  echo "Error creating BASE_DIR."
+  exit 1
+fi
+
+# Step 2: Create home folder if not exists
+if [ ! -d "$HOME_DIR" ]; then
+  mkdir -p "$HOME_DIR"
+fi
+   
+# Step 3: Download conty.sh with download percentage indicator
+wget batocera.pro/app/conty.sh -O ~/pro/steam/conty.sh 
+
+
+# Step 4: Make conty.sh executable
+chmod +x "$DOWNLOAD_FILE"
+
+killall -9 vlc
+echo "DONE"
+sleep 5
