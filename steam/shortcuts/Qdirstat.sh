12a3d9fa95d718413e7ba5bebc04d9856448b4b2
uureel <116395185+uureel@users.noreply.github.com>
2024-02-07 23:51:52 +0100

    +

--- steam/shortcuts/Qdirstat.sh
+++ steam/shortcuts/Qdirstat.sh
@@ -7,4 +7,4 @@ chmod -R g+rwX /var/run/pulse
 
 unclutter-remote -s
 
-su - batocera -c "HOME_DIR=\"/userdata/system/pro/steam/home\" DISPLAY=:0.0 ~/pro/steam/conty.sh qdirstat"
+ALLOW_ROOT=1 DISPLAY=:0.0 HOME_DIR=/userdata/system/pro/steam/home ~/pro/steam/conty.sh qdirstat "${@}"
