6b21f25865e1fe8cb6b51cf2d84e37370712163b
uureel <116395185+uureel@users.noreply.github.com>
2024-02-07 23:42:26 +0100

    +

--- steam/shortcuts/Steam Big Picture Mode.sh
+++ steam/shortcuts/Steam Big Picture Mode.sh
@@ -14,4 +14,4 @@ mkdir -p $dir1 $dir2 $dir3 2>/dev/null
 chown -R batocera:batocera $dir1 $dir2 $dir3 2>/dev/null
 
 #su - batocera -c "dbus-launch --sh-syntax & DISPLAY=:0.0 HOME_DIR=/userdata/system/pro/steam/home ~/pro/steam/conty.sh dbus-run-session steam -gamepadui"
-ALLOW_ROOT=1 DISPLAY=:0.0 /userdata/system/pro/steam/conty.sh dbus-run-session steam -gamepadui
+ALLOW_ROOT=1 DISPLAY=:0.0 /userdata/system/pro/steam/conty.sh dbus-run-session steam -gamepadui "${@}"
