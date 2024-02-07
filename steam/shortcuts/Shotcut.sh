2d4de0ee76aa16ecf10e260564123227f1c3df0e
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-07 11:28:42 +0200

    Add files via upload

---
+++ steam/shortcuts/Shotcut.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p ~/.local
+chmod 777 ~/.local
+chmod 777 ~/.local/*
+chown -R root:audio /var/run/pulse
+chmod -R g+rwX /var/run/pulse
+
+unclutter-remote -s
+
+dir1=/userdata/system/pro/steam/home
+dir2=/userdata/system/.local/share/Conty
+dir3=/userdata/system/.cache
+mkdir -p $dir1 $dir2 $dir3 2>/dev/null
+chown -R batocera:batocera $dir1 $dir2 $dir3 2>/dev/null
+
+eval $(dbus-launch --sh-syntax)
+su - batocera -c "HOME_DIR=\"/userdata/system/pro/steam/home\" DISPLAY=:0.0 ~/pro/steam/conty.sh dbus-run-session shotcut"
