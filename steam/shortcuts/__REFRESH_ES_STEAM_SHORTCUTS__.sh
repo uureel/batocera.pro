b144a41c696a70a7796b9a9eb6c9104ff11e31f9
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-08 00:17:07 +0200

    Create __REFRESH_ES_STEAM_SHORTCUTS__.sh

---
+++ steam/shortcuts/__REFRESH_ES_STEAM_SHORTCUTS__.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Specify the directory to scan and the output file
+directory_to_scan="/userdata/system/pro/steam/home/.local/share/applications"
+output_file="/userdata/system/pro/steam/steam_list.txt"
+
+# Check if the directory exists
+if [ ! -d "$directory_to_scan" ]; then
+    echo "Error: Directory '$directory_to_scan' does not exist."
+    exit 1
+fi
+
+# Remove existing output file
+rm -f "$output_file"
+
+# Iterate through .desktop files in the specified directory
+for file_path in "$directory_to_scan"/*.desktop; do
+    # Check if the file exists
+    if [ -f "$file_path" ]; then
+        # Extract the filename without extension
+        filename=$(basename "$file_path" .desktop)
+
+        # Extract the relevant information using grep and sed
+        exec_line=$(grep '^Exec=steam steam://rungameid/' "$file_path" | sed -n "s/^Exec=steam steam:\/\/rungameid\/\([0-9]\+\)/$filename steam steam:\/\/rungameid\/\1/p")
+        if [ -n "$exec_line" ]; then
+            # Append the information to the output file
+            echo "$exec_line" >> "$output_file"
+        fi
+    fi
+done
+
+
+
+# Full path to steam_list.txt
+steam_list_file="/userdata/system/pro/steam/steam_list.txt"
+
+# Read each line from steam_list.txt
+while IFS= read -r line; do
+    # Extract game_name and steam_url_part
+    game_name=$(echo "$line" | awk -F ' steam ' '{print $1}')
+    steam_url_part=$(echo "$line" | awk -F ' steam ' '{print $2}')
+
+    # Create the script content
+    script_content="#!/bin/bash
+mkdir -p ~/.local
+chmod 777 ~/.local
+chmod 777 ~/.local/*
+chown -R root:audio /var/run/pulse
+chmod -R g+rwX /var/run/pulse
+
+unclutter-remote -s
+
+dir1=/userdata/system/pro/steam/home
+dir2=/userdata/system/.local/share/Conty
+dir3=/userdata/system/.cache
+mkdir -p $dir1 $dir2 $dir3 2>/dev/null
+chown -R batocera:batocera $dir1 $dir2 $dir3 2>/dev/null
+
+su - batocera -c \"HOME_DIR=\\\"/userdata/system/pro/steam/home\\\" DISPLAY=:0.0 ~/pro/steam/conty.sh steam $steam_url_part -gamepadui\""
+
+    # Create the script file
+    script_path="/userdata/roms/steam2/$game_name.sh"
+    echo "$script_content" > "$script_path"
+    chmod +x "$script_path"
+
+    echo "Script created: $script_path"
+
+
+echo "Script execution completed. Check $output_file for the result."
+done < "$steam_list_file"
+
+killall -9 emulationstation
