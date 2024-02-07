4851a6c585eaeb11970aa565cadc0ec431401d5d
uureel <116395185+uureel@users.noreply.github.com>
2024-02-07 23:53:59 +0100

    +

--- steam/shortcuts/Balena-Etcher.sh
+++ steam/shortcuts/Balena-Etcher.sh
@@ -7,4 +7,5 @@ chmod -R g+rwX /var/run/pulse
 
 unclutter-remote -s
 
-su - batocera -c "HOME_DIR=\"/userdata/system/pro/steam/home\" DISPLAY=:0.0 ~/pro/steam/conty.sh /opt/balenaEtcher/balena-etcher %U"
+#su - batocera -c "HOME_DIR=\"/userdata/system/pro/steam/home\" DISPLAY=:0.0 ~/pro/steam/conty.sh /opt/balenaEtcher/balena-etcher %U"
+ALLOW_ROOT=1 DISPLAY=:0.0 HOME_DIR=/userdata/system/pro/steam/home ~/pro/steam/conty.sh /opt/balenaEtcher/balena-etcher "${@}"
--- steam/shortcuts/Gparted.sh
+++ steam/shortcuts/Gparted.sh
@@ -7,4 +7,4 @@ chmod -R g+rwX /var/run/pulse
 
 unclutter-remote -s
 
-ALLOW_ROOT=1  HOME_DIR=\"/userdata/system/pro/steam/home\" DISPLAY=:0.0 ~/pro/steam/conty.sh /usr/bin/gparted
+ALLOW_ROOT=1  HOME_DIR=/userdata/system/pro/steam/home DISPLAY=:0.0 ~/pro/steam/conty.sh /usr/bin/gparted
