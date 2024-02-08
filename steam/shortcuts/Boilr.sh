4df859dd292922779c5c2150b44684e9764c83a3
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-08 14:04:04 +0200

    Update Boilr.sh

--- steam/shortcuts/Boilr.sh
+++ steam/shortcuts/Boilr.sh
@@ -6,5 +6,5 @@ chown -R root:audio /var/run/pulse
 chmod -R g+rwX /var/run/pulse
 
 unclutter-remote -s
-
-su - batocera -c "HOME_DIR=\"/userdata/system/pro/steam/home\" DISPLAY=:0.0 ~/pro/steam/conty.sh boilr-gui"
+ALLOW_ROOT=1 DISPLAY=:0.0 ~/pro/steam/conty.sh dbus-run-session  boilr-gui "${@}"
+#su - batocera -c "HOME_DIR=\"/userdata/system/pro/steam/home\" DISPLAY=:0.0 ~/pro/steam/conty.sh boilr-gui"
