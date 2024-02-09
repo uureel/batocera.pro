074a81da121b2b95ac14b0cdb89983a2c23ced4f
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-09 17:06:18 +0200

    +

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -403,9 +403,6 @@ touch "${bootstrap}"/etc/asound.conf
 touch "${bootstrap}"/etc/localtime
 chmod 755 "${bootstrap}"/root
 
-# Enable full font hinting
-rm -f "${bootstrap}"/etc/fonts/conf.d/10-hinting-slight.conf
-ln -s /usr/share/fontconfig/conf.avail/10-hinting-full.conf "${bootstrap}"/etc/fonts/conf.d
 
 clear
 
@@ -416,14 +413,6 @@ sed -i 's,os.geteuid() == 0,os.geteuid() == 888,g' "${bootstrap}"/usr/lib/python
 sed -i 's/geteuid/getppid/' "${bootstrap}"/usr/sbin/vlc 2>/dev/null
 
 
-# --
-# Include dbus fix (dbus session)
-dbus="${bootstrap}/usr/bin/dbus"
-	rm "$dbus" 2>/dev/null
-		echo '#!/bin/bash' >> "$dbus"
-		echo 'eval "$(dbus-launch --sh-syntax)' >> "$dbus"
-		dos2unix "$dbus" 2>/dev/null && chmod 777 "$dbus" 2>/dev/null
-		chown -R batocera:batocera "$dbus" 2>/dev/null
 
 echo "Done"
 
@@ -442,58 +431,7 @@ if [ -f "${bootstrap}"/opt/bad_aur_pkglist.txt ]; then
 	rm "${bootstrap}"/opt/bad_aur_pkglist.txt
 fi
 
-######################
-######################
-##                  ##
-##   ENTER CHROOT   ##
-##                  ##
-######################
-######################
 
-# Root rights are required
-
-if [ $EUID != 0 ]; then
-    echo "Root rights are required!"
-
-    exit 1
-fi
-
-script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-
-bootstrap="${script_dir}"/root.x86_64
-
-if [ ! -d "${bootstrap}" ]; then
-    echo "Bootstrap is missing"
-    exit 1
-fi
-
-# First unmount just in case
-umount -Rl "${bootstrap}"
-
-mount --bind "${bootstrap}" "${bootstrap}"
-mount -t proc /proc "${bootstrap}"/proc
-mount --bind /sys "${bootstrap}"/sys
-mount --make-rslave "${bootstrap}"/sys
-mount --bind /dev "${bootstrap}"/dev
-mount --bind /dev/pts "${bootstrap}"/dev/pts
-mount --bind /dev/shm "${bootstrap}"/dev/shm
-mount --make-rslave "${bootstrap}"/dev
-
-rm -f "${bootstrap}"/etc/resolv.conf
-cp /etc/resolv.conf "${bootstrap}"/etc/resolv.conf
-
-mkdir -p "${bootstrap}"/run/shm
-
-echo "Entering chroot"
-
-# ------------------------------------------------------------------------------------------
-# REBUILD LIBC WITH DT_HASH PATCH
-chroot "${bootstrap}" \
-/usr/bin/env LANG=en_US.UTF-8 TERM=xterm PATH="/bin:/sbin:/usr/bin:/usr/sbin" /bin/bash -c \
-"curl -Ls https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/build/libc-dthash-patch.sh | bash && exit"
-# ------------------------------------------------------------------------------------------
-
-echo "Exiting chroot"
 
 umount -l "${bootstrap}"
 umount "${bootstrap}"/proc
