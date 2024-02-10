5d1f9e8534702bbea3247867fcb6cef222d61905
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-10 23:07:05 +0200

    Create Opera.sh

---
+++ steam/shortcuts/Opera.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+home=/userdata/system/pro/steam/home
+mkdir -p ~/.local /home $home 2>/dev/null
+ln -s /userdata/system /home/root 2>/dev/null
+chmod -R 777 /var/run/pulse
+chmod 777 ~/.local/*
+chmod 777 ~/.local
+chown root:root $home 2>/dev/null
+chown root:root /home 2>/dev/null
+chown root:root /home/root 2>/dev/null
+chown root:root /home/root/.config 2>/dev/null
+
+unclutter-remote -s
+
+ALLOW_ROOT=1 DISPLAY=:0.0 ~/pro/steam/conty.sh dbus-run-session /usr/lib/firefox/firefox "${@}"
