72a12a9f5d53c95c296d890d217824c2f3513e37
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-14 14:46:25 +0200

    Update +UPDATE-LUTRIS-SHORTCUTS.sh

--- steam/shortcuts/+UPDATE-LUTRIS-SHORTCUTS.sh
+++ steam/shortcuts/+UPDATE-LUTRIS-SHORTCUTS.sh
@@ -38,10 +38,26 @@ while IFS= read -r line; do
 
     # Create the script content without an undesired trailing quote
     script_content="#!/bin/bash
- 
+home=/userdata/system/pro/steam/home
+mkdir -p ~/.local /home $home 2>/dev/null
+ln -s /userdata/system /home/root 2>/dev/null
+chmod -R 777 /var/run/pulse
+chmod 777 ~/.local/*
+chmod 777 ~/.local
+
+dir1=/userdata/system/pro/steam/home
+dir2=/userdata/system/.local/share/Conty
+dir3=/userdata/system/.cache
+mkdir -p $dir1 $dir2 $dir3 2>/dev/null
+chown -R batocera:batocera $dir1 $dir2 $dir3 2>/dev/null
+
+eval $(dbus-launch --sh-syntax)
+sysctl -w vm.max_map_count=2147483642
+ulimit -H -n 819200
+ulimit -S -n 819200
 unclutter-remote -s
 ALLOW_ROOT=1 DISPLAY=:0.0 ~/pro/steam/conty.sh env LUTRIS_SKIP_INIT=1 lutris lutris:$lutris_url_part"
-unclutter-remote h
+unclutter-remote -h
 
     # Create the script file
     script_path="/userdata/roms/lutris/$game_name.sh"
