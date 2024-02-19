a79c540e45bdf0b1505069f782755a0ff202866d
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-19 14:10:20 +0200

    Update gpu.sh

--- steam/gpu.sh
+++ steam/gpu.sh
@@ -17,30 +17,13 @@ clear
 # Act based on the user choice
 case $CHOICE in
     1)
-        echo "Installing Steam Container..."
+        echo "Loading AMD/INTEL SCRIPT"
         curl -L https://github.com/uureel/batocera.pro/raw/main/steam/nv_warning.sh | bash
         ;;
     2)
-        echo "Loading Uninstall script..."
-        curl -L https://github.com/uureel/batocera.pro/raw/main/steam/uninstall.sh | bash
+        echo "Loading NVIDIA script..."
+        curl -L https://github.com/uureel/batocera.pro/raw/main/steam/nv_menu.sh | bash
         ;;
-    3)
-        echo "Update EmulationStation Arch Container Launcher Shortcuts..."
-        rm /userdata/roms/conty/*.sh
-        curl -L https://github.com/uureel/batocera.pro/raw/main/steam/update_shortcuts.sh | bash
-        ;;    
-    4)  echo "Update/Re-download Container..."
-        curl -L https://github.com/uureel/batocera.pro/raw/main/steam/redownload.sh  | bash
-        ;;
-    5)  echo "Update Conty Container..."
-        curl -L https://github.com/uureel/batocera.pro/raw/main/steam/upgrade.sh  | bash
-        ;;
-    6) echo "Add Lutris shortcuts to emulationstation..."
-       curl -L https://github.com/uureel/batocera.pro/raw/main/steam/addon_lutris.sh | bash
-       ;;
-    7) echo "Add Heroic shortcuts to emulationstation..."
-       curl -L https://github.com/uureel/batocera.pro/raw/main/steam/addon_heroic.sh | bash
-       ;;
     *)
         echo "No valid option selected or cancelled. Exiting."
         ;;
