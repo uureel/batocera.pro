f72ba3e1ddb5ffb6a80cbcaf8cd95a91035d21f4
uureel <116395185+uureel@users.noreply.github.com>
2024-02-20 12:26:22 +0100

    conty big update

---
+++ steam/build/applauncher.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#------------------------------------------------
+conty=/userdata/system/pro/steam/conty.sh
+#------------------------------------------------
+# show mouse
+  unclutter-remote -s
+#------------------------------------------------
+# run
+"$conty" \
+--bind /userdata/system/etc/passwd /etc/passwd \
+--bind /userdata/system/etc/group /etc/group \
+--bind /var/run/nvidia /var/run/nvidia \
+--bind /sys/fs/cgroup /sys/fs/cgroup \
+--bind /etc/fonts /etc/fonts \
+--bind /userdata /userdata \
+--bind /newroot /newroot \
+--bind / /batocera \
+bash -c "prepare && fish ${@}"
+#------------------------------------------------
+# hide mouse
+  unclutter-remote -h
+#------------------------------------------------
