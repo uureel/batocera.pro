3afe6656c458e3c5301b99276b7a7c8ff76b50ad
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-21 11:18:19 +0200

    Update +UPDATE-LUTRIS-SHORTCUTS.sh

--- steam/shortcuts/+UPDATE-LUTRIS-SHORTCUTS.sh
+++ steam/shortcuts/+UPDATE-LUTRIS-SHORTCUTS.sh
@@ -45,26 +45,33 @@ while IFS= read -r line; do
 
     # Create the script content without an undesired trailing quote
     script_content="#!/bin/bash
-home=/userdata/system/pro/steam/home
-mkdir -p ~/.local /home $home 2>/dev/null
-ln -s /userdata/system /home/root 2>/dev/null
-chmod -R 777 /var/run/pulse
-chmod 777 ~/.local/*
-chmod 777 ~/.local
+    #------------------------------------------------
+conty=/userdata/system/pro/steam/conty.sh
+#------------------------------------------------
+batocera-mouse show
+killall -9 steam steamfix steamfixer 2>/dev/null
+#------------------------------------------------
+/userdata/system/pro/steam/conty.sh \
+          --bind /userdata/system/containers/storage /var/lib/containers/storage \\
+          --bind /userdata/system/flatpak /var/lib/flatpak \\
+          --bind /userdata/system/etc/passwd /etc/passwd \\
+          --bind /userdata/system/etc/group /etc/group \\
+          --bind /var/run/nvidia /var/run/nvidia \\
+          --bind /userdata/system /home/batocera \\
+          --bind /sys/fs/cgroup /sys/fs/cgroup \\
+          --bind /userdata/system /home/root \\
+          --bind /etc/fonts /etc/fonts \\
+          --bind /userdata /userdata \\
+          --bind /newroot /newroot \\
+          --bind / /batocera \\
+    bash -c 'prepare && dbus-run-session env LUTRIS_SKIP_INIT=1 lutris lutris:$lutris_url_part '"${@}"''
+#------------------------------------------------
+batocera-mouse hide
+#----------------
+"
+ 
+   #  LUTRIS COMMAND env LUTRIS_SKIP_INIT=1 lutris lutris:$lutris_url_part
 
-dir1=/userdata/system/pro/steam/home
-dir2=/userdata/system/.local/share/Conty
-dir3=/userdata/system/.cache
-mkdir -p $dir1 $dir2 $dir3 2>/dev/null
-chown -R batocera:batocera $dir1 $dir2 $dir3 2>/dev/null
-
-eval $(dbus-launch --sh-syntax)
-sysctl -w vm.max_map_count=2147483642
-ulimit -H -n 819200
-ulimit -S -n 819200
-unclutter-remote -s
-ALLOW_ROOT=1 DISPLAY=:0.0 ~/pro/steam/conty.sh env LUTRIS_SKIP_INIT=1 lutris lutris:$lutris_url_part"
-unclutter-remote -h
 
     # Create the script file
     script_path="/userdata/roms/lutris/$game_name.sh"
