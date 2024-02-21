07c875702547d6d0f5866994e20e284b73113363
uureel <116395185+uureel@users.noreply.github.com>
2024-02-21 18:52:49 +0100

    +

--- steam/shortcuts/Heroic Game Launcher.sh
+++ steam/shortcuts/Heroic Game Launcher.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && ulimit -H -n 819200 && ulimit -S -n 819200 && dbus-run-session heroic --no-sandbox --test-type '"${@}"''
+  bash -c 'prepare && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session heroic --no-sandbox --test-type '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
