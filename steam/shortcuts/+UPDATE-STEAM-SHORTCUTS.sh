d7d7561ec42dc6724ac8c144176fd9870cba6f85
uureel <116395185+uureel@users.noreply.github.com>
2024-02-21 11:33:45 +0100

    Update +UPDATE-STEAM-SHORTCUTS.sh

--- steam/shortcuts/+UPDATE-STEAM-SHORTCUTS.sh
+++ steam/shortcuts/+UPDATE-STEAM-SHORTCUTS.sh
@@ -38,27 +38,25 @@ while IFS= read -r line; do
 
     # Create the script content without an undesired trailing quote
     script_content="#!/bin/bash
-
-#------------------------------------------------
-conty=/userdata/system/pro/steam/conty.sh
 #------------------------------------------------
 batocera-mouse show
+#------------------------------------------------
 killall -9 steam steamfix steamfixer 2>/dev/null
 #------------------------------------------------
-/userdata/system/pro/steam/conty.sh \
---bind /userdata/system/containers/storage /var/lib/containers/storage \
---bind /userdata/system/flatpak /var/lib/flatpak \
---bind /userdata/system/etc/passwd /etc/passwd \
---bind /userdata/system/etc/group /etc/group \
---bind /var/run/nvidia /var/run/nvidia \
---bind /userdata/system /home/batocera \
---bind /sys/fs/cgroup /sys/fs/cgroup \
---bind /userdata/system /home/root \
---bind /etc/fonts /etc/fonts \
---bind /userdata /userdata \
---bind /newroot /newroot \
---bind / /batocera \
-bash -c 'prepare && dbus-run-session steam $steam_url_part -gamepadui '"\${@}"''
+/userdata/system/pro/steam/conty.sh \\
+--bind /userdata/system/containers/storage /var/lib/containers/storage \\
+--bind /userdata/system/flatpak /var/lib/flatpak \\
+--bind /userdata/system/etc/passwd /etc/passwd \\
+--bind /userdata/system/etc/group /etc/group \\
+--bind /var/run/nvidia /var/run/nvidia \\
+--bind /userdata/system /home/batocera \\
+--bind /sys/fs/cgroup /sys/fs/cgroup \\
+--bind /userdata/system /home/root \\
+--bind /etc/fonts /etc/fonts \\
+--bind /userdata /userdata \\
+--bind /newroot /newroot \\
+--bind / /batocera \\
+bash -c 'prepare && dbus-run-session steam $steam_url_part -gamepadui'
 #------------------------------------------------
 batocera-mouse hide
 #------------------------------------------------
