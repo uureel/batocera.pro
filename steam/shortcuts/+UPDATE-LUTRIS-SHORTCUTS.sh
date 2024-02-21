1740655ca2a4fe27b33969d8368e3874460cdf66
uureel <116395185+uureel@users.noreply.github.com>
2024-02-21 11:14:03 +0100

    Update +UPDATE-LUTRIS-SHORTCUTS.sh

--- steam/shortcuts/+UPDATE-LUTRIS-SHORTCUTS.sh
+++ steam/shortcuts/+UPDATE-LUTRIS-SHORTCUTS.sh
@@ -45,29 +45,26 @@ while IFS= read -r line; do
 
     # Create the script content without an undesired trailing quote
     script_content="#!/bin/bash
-    #------------------------------------------------
-conty=/userdata/system/pro/steam/conty.sh
 #------------------------------------------------
 batocera-mouse show
-killall -9 steam steamfix steamfixer 2>/dev/null
 #------------------------------------------------
 /userdata/system/pro/steam/conty.sh \
-          --bind /userdata/system/containers/storage /var/lib/containers/storage \\
-          --bind /userdata/system/flatpak /var/lib/flatpak \\
-          --bind /userdata/system/etc/passwd /etc/passwd \\
-          --bind /userdata/system/etc/group /etc/group \\
-          --bind /var/run/nvidia /var/run/nvidia \\
-          --bind /userdata/system /home/batocera \\
-          --bind /sys/fs/cgroup /sys/fs/cgroup \\
-          --bind /userdata/system /home/root \\
-          --bind /etc/fonts /etc/fonts \\
-          --bind /userdata /userdata \\
-          --bind /newroot /newroot \\
-          --bind / /batocera \\
-    bash -c 'prepare && dbus-run-session env LUTRIS_SKIP_INIT=1 lutris lutris:$lutris_url_part '"${@}"''
+--bind /userdata/system/containers/storage /var/lib/containers/storage \
+--bind /userdata/system/flatpak /var/lib/flatpak \
+--bind /userdata/system/etc/passwd /etc/passwd \
+--bind /userdata/system/etc/group /etc/group \
+--bind /var/run/nvidia /var/run/nvidia \
+--bind /userdata/system /home/batocera \
+--bind /sys/fs/cgroup /sys/fs/cgroup \
+--bind /userdata/system /home/root \
+--bind /etc/fonts /etc/fonts \
+--bind /userdata /userdata \
+--bind /newroot /newroot \
+--bind / /batocera \
+bash -c 'prepare && dbus-run-session env LUTRIS_SKIP_INIT=1 lutris lutris:$lutris_url_part '"\${@}"''
 #------------------------------------------------
 batocera-mouse hide
-#----------------
+#------------------------------------------------
 "
  
    #  LUTRIS COMMAND env LUTRIS_SKIP_INIT=1 lutris lutris:$lutris_url_part
