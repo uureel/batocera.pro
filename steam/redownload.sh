dcf38ba1f7417e0afebcb56a1576a5f5d7332a9d
uureel <116395185+uureel@users.noreply.github.com>
2024-02-21 14:43:53 +0100

    Update redownload.sh

--- steam/redownload.sh
+++ steam/redownload.sh
@@ -38,6 +38,10 @@ rm /userdata/system/pro/steam/conty.sh 2>/dev/null
 wget -q --show-progress --tries=10 --no-check-certificate --no-cache --no-cookies -O /userdata/system/pro/steam/conty.sh http://batocera.pro/app/conty.sh 
 chmod 777 /userdata/system/pro/steam/conty.sh 2>/dev/null
 
+wget -q --tries=10 --no-check-certificate --no-cache --no-cookies -O /userdata/system/pro/steam/batocera-conty-patcher.sh https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/build/batocera-conty-patcher.sh
+dos2unix /userdata/system/pro/steam/batocera-conty-patcher.sh 2>/dev/null
+chmod 777 /userdata/system/pro/steam/batocera-conty-patcher.sh 2>/dev/null
+
 killall -9 vlc
 echo "DONE"
 sleep 5
