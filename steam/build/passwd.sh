1a2823e782b02e0ba2c394a9b53bc09da575ba2e
uureel <116395185+uureel@users.noreply.github.com>
2024-02-22 23:19:51 +0100

    +
    +

---
+++ steam/build/passwd.sh
@@ -0,0 +1,38 @@
+root:x:0:0::/root:/usr/bin/bash
+bin:x:1:1::/:/usr/bin/nologin
+daemon:x:2:2::/:/usr/bin/nologin
+mail:x:8:12::/var/spool/mail:/usr/bin/nologin
+ftp:x:14:11::/srv/ftp:/usr/bin/nologin
+http:x:33:33::/srv/http:/usr/bin/nologin
+nobody:x:65534:65534:Kernel Overflow User:/:/usr/bin/nologin
+dbus:x:81:81:System Message Bus:/:/usr/bin/nologin
+systemd-coredump:x:980:980:systemd Core Dumper:/:/usr/bin/nologin
+systemd-network:x:979:979:systemd Network Management:/:/usr/bin/nologin
+systemd-oom:x:978:978:systemd Userspace OOM Killer:/:/usr/bin/nologin
+systemd-journal-remote:x:977:977:systemd Journal Remote:/:/usr/bin/nologin
+systemd-resolve:x:976:976:systemd Resolver:/:/usr/bin/nologin
+systemd-timesync:x:975:975:systemd Time Synchronization:/:/usr/bin/nologin
+tss:x:974:974:tss user for tpm2:/:/usr/bin/nologin
+uuidd:x:68:68::/:/usr/bin/nologin
+avahi:x:967:967:Avahi mDNS/DNS-SD daemon:/:/usr/bin/nologin
+flatpak:x:966:966:Flatpak system helper:/:/usr/bin/nologin
+geoclue:x:965:965:Geoinformation service:/var/lib/geoclue:/usr/bin/nologin
+git:x:964:964:git daemon user:/:/usr/bin/git-shell
+nixbld01:x:963:970:Nix builder 01:/:/usr/bin/nologin
+nixbld02:x:962:970:Nix builder 02:/:/usr/bin/nologin
+nixbld03:x:961:970:Nix builder 03:/:/usr/bin/nologin
+nixbld04:x:960:970:Nix builder 04:/:/usr/bin/nologin
+nixbld05:x:959:970:Nix builder 05:/:/usr/bin/nologin
+nixbld06:x:958:970:Nix builder 06:/:/usr/bin/nologin
+nixbld07:x:957:970:Nix builder 07:/:/usr/bin/nologin
+nixbld08:x:956:970:Nix builder 08:/:/usr/bin/nologin
+nixbld09:x:955:970:Nix builder 09:/:/usr/bin/nologin
+nixbld10:x:954:970:Nix builder 10:/:/usr/bin/nologin
+nvidia-persistenced:x:143:143:NVIDIA Persistence Daemon:/:/usr/bin/nologin
+polkitd:x:102:102:User for polkitd:/:/usr/bin/nologin
+rtkit:x:133:133:RealtimeKit:/proc:/usr/bin/nologin
+usbmux:x:140:140:usbmux user:/:/usr/bin/nologin
+aur:x:1000:1000::/home/aur:/usr/bin/bash
+libvirt-qemu:x:952:952:Libvirt QEMU user:/:/usr/bin/nologin
+qemu:x:951:951:QEMU user:/:/usr/bin/nologin
+dnsmasq:x:950:950:dnsmasq daemon:/:/usr/bin/nologin
