67f61ce544d9465e7071e9b30f2e163ee6ac6be0
uureel <116395185+uureel@users.noreply.github.com>
2024-02-24 17:05:41 +0100

    Update portproton.sh

--- steam/build/portproton.sh
+++ steam/build/portproton.sh
@@ -4,4 +4,5 @@ if [[ -e ~/.config/PortProton.conf ]]; then
 	path=$(cat ~/.config/PortProton.conf | grep "/" | head -n1)
 	sed -i 's,(id -u),(fakeid -u),g' "$path/data/scripts/runlib" 2>/dev/null
 fi
+ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1
 dbus-run-session /usr/bin/properportproton "${@}"
