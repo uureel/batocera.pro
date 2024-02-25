a55da8b4789e0b1213db84b63953a13121df9201
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-25 18:24:37 +0200

    Update download.sh

--- emudeck/download.sh
+++ emudeck/download.sh
@@ -63,6 +63,7 @@ done
 echo "Download complete."
 sleep 3
 echo "Starting Emudeck -- Check your main display -- Please install Emudeck, then close out Emudeck to continue installation"
+sleeo 10
 /userdata/roms/conty/Emudeck.sh
 
 echo "Continuing install"
