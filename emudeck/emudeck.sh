c9174d782fb3f5ca9ba5837d2b8460c2ee43ac32
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-25 18:40:22 +0200

    Create emudeck.sh

---
+++ emudeck/emudeck.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define the options
+OPTIONS=("1" "Install Emudeck"
+         "2" "(Re)Download Steam Rom Manager Parser fix")
+         
+# Display the dialog and get the user choice
+CHOICE=$(dialog --clear --backtitle "Emudeck Manager" \
+                --title "Main Menu" \
+                --menu "Choose an option:" 15 75 3 \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+
+# Act based on the user choice
+case $CHOICE in
+    1)
+        echo "Installing emudeck..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/install2.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    2)
+        echo Parser Fix..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/uninstall.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    *)
+        echo "No valid option selected or cancelled. Exiting."
+        ;;
+esac
