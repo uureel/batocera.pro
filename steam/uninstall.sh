71ef49f79ef3c3de5fab4d3d811f4f007ffe710c
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-02-26 22:21:16 +0200

    Update uninstall.sh

--- steam/uninstall.sh
+++ steam/uninstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Introductory message using dialog
-dialog --title "Warning" --msgbox "This script will uninstall the arch container and delete data in the following:\n\n1. ~/.local/share/Conty \n2. ~/pro/steam \n\nThere may be saves." 15 60
+dialog --title "Warning" --msgbox "This script will uninstall the arch container and delete data in the following:\n\n1. ~/.local/share/Conty \n2. ~/pro/steam
+\n3. /userdata/roms/conty \n4. /userdata/roms/lutris \n5. /userdata/roms/heroic2 \n6. /userdata/roms/steam2   \n\nThere may be saves." 15 60
 
 # Confirmation dialog
 if dialog --title "Confirm" --yesno "Do you want to proceed the uninstall and delete the folders? The container's data in ~/pro/steam/home if exists will be deleted!!!  Select 'yes' to continue." 10 60; then
