d4058638eea267dd7812852db6227ddb3c3bee87
uureel <116395185+uureel@users.noreply.github.com>
2024-03-03 10:57:26 +0100

    +
    +

--- steam/shortcuts/Steam Big Picture Mode.sh
+++ steam/shortcuts/Steam Big Picture Mode.sh
@@ -18,9 +18,7 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /etc/fonts /etc/fonts \
           --bind /userdata /userdata \
           --bind /newroot /newroot \
-          --bind /dev/dri /dev/dri \
           --bind / /batocera \
-          --bind /sys /sys \
   bash -c 'prepare && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session steam -gamepadui '"${@}"''
 #------------------------------------------------
 batocera-mouse hide
