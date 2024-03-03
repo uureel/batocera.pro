5d8048841581c27673bc573c1de97645c5ae919d
uureel <116395185+uureel@users.noreply.github.com>
2024-03-03 11:14:28 +0100

    +
    +

--- steam/shortcuts/Steam Big Picture Mode.sh
+++ steam/shortcuts/Steam Big Picture Mode.sh
@@ -14,11 +14,13 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata/system /home/batocera \
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
-          --bind /dev/input /dev/input \
           --bind /etc/fonts /etc/fonts \
+          --bind /dev/input /dev/input \
           --bind /userdata /userdata \
+          --bind /dev/dri /dev/dri \
           --bind /newroot /newroot \
           --bind / /batocera \
+          --bind /sys /sys \
   bash -c 'prepare && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session steam -gamepadui '"${@}"''
 #------------------------------------------------
 batocera-mouse hide
--- steam/shortcuts/Steam Diagnostic.sh
+++ steam/shortcuts/Steam Diagnostic.sh
@@ -14,11 +14,13 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata/system /home/batocera \
           --bind /sys/fs/cgroup /sys/fs/cgroup \
           --bind /userdata/system /home/root \
-          --bind /dev/input /dev/input \
           --bind /etc/fonts /etc/fonts \
+          --bind /dev/input /dev/input \
           --bind /userdata /userdata \
+          --bind /dev/dri /dev/dri \
           --bind /newroot /newroot \
           --bind / /batocera \
+          --bind /sys /sys \
   xterm -fs 12 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c 'prepare && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && DISPLAY=:0.0 dbus-run-session steam'
 #------------------------------------------------
 # batocera-mouse hide
