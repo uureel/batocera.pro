3ba7eacdc2ef03c79a55a15a611e68ef34c187d5
trashbus99 <109239364+trashbus99@users.noreply.github.com>
2024-03-04 23:27:33 +0200

    Update create.sh

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -64,7 +64,8 @@ export aur_packagelist="heroic-games-launcher-bin geforcenow-electron moonlight-
 etcher-bin ventoy-bin microsoft-edge-stable-bin qdirstat peazip-gtk2-bin appimagetool-bin \
 protonup-qt-bin steam-rom-manager-bin google-chrome sgdboop-bin steam-boilr-gui zoom chiaki \
 winegui-bin protontricks steamtinkerlaunch greenlight-beta-appimage portproton btrfs-assistant \
-7-zip-bin antimicrox lutris-git sublime-text-4 freefilesync"
+7-zip-bin antimicrox lutris-git sublime-text-4 freefilesync yuzu-early-access yuzu-mainline-bin \
+ryujinx-bin ryujinx-ldn-bin ryujinx-ava-bin citra-canary-bin citra-nightly-bin"
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
