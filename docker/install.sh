5f098010247c4510f38fc5a999b5d695bd7d3eeb
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-04-07 17:46:44 +0300

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-# Define the final CLI directory
-final_cli_dir="/userdata/system/cli"
+# Define variables
+FILE_ID="1ZGLOM7KeAHg1ZbJXRsotLXHw2ZlGjz4u"
+DESTINATION_DIR="$HOME/cli"
+FILENAME="cli2.tar.gz"
+DOWNLOAD_URL="https://drive.usercontent.google.com/download?id=${FILE_ID}&confirm=t"
 
-# Create a temporary directory under /userdata/system
-temp_dir="/userdata/system/temp_cli"
-mkdir -p "${temp_dir}"
+# Create the destination directory if it doesn't exist
+mkdir -p "$DESTINATION_DIR"
 
-# Step 1: Download and extract cli.tar.gz into the temporary directory
-wget -O "${temp_dir}/cli.tar.gz" https://batocera.pro/app/cli.tar.gz
-tar -xf "${temp_dir}/cli.tar.gz" -C "${temp_dir}"
+# Download the file using curl
+curl -L "${DOWNLOAD_URL}" -o "${FILENAME}"
 
-# Move the extracted CLI content to the final directory
-mkdir -p "${final_cli_dir}"
-mv "${temp_dir}"/* "${final_cli_dir}/"
+# Extract the contents to the destination directory
+tar -xzvf "${FILENAME}" -C "${DESTINATION_DIR}"
 
-# Ensure the run script is executable
-chmod +x "${final_cli_dir}/run"
 
-# Step 2: Create directory if it doesn't exist
-mkdir -p /userdata/system/services
+# Modify the run script: change enter_zsh=1 to enter_zsh=0
+sed -i 's/enter_zsh=1/enter_zsh=0/' "${DESTINATION_DIR}/run"
 
-# Step 3: Download docker script, move it to the target directory, and make it executable
-wget https://github.com/uureel/batocera.pro/raw/main/docker/docker -O /userdata/system/services/docker
-chmod +x /userdata/system/services/docker
+#wget https://github.com/garbagescow/batocera.pro/raw/main/docker/docker -O /userdata/system/services/docker
+#chmod +x /userdata/system/services/docker
 
-# Step 4: Run the CLI run script from its final location
-"${final_cli_dir}/run"
+# Add the command to ~/custom.sh before starting Docker and Portainer
+echo "bash /userdata/system/cli/run &" >> ~/custom.sh
+
+cd ~/cli
+chmod +x run
+
+clear
+echo "Starting Docker.."
+~/cli/run
+
+clear
+
+
 
 # Step 5: Install Portainer
+echo "Installing portainer"
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
-# Clean up: Remove the temporary directory
-rm -rf "${temp_dir}"
+echo "Done." 
+echo "Access portainer gui via https://<batoceraipaddress>:9443"
+sleep 10
+curl http://127.0.0.1:1234/reloadgames
