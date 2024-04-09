f40be1a03bb898cf6186c8e4b1d36addf3d04e6e
uureel <116395185+uureel@users.noreply.github.com>
2024-04-09 17:18:19 +0200

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -8,13 +8,11 @@ if [ "$architecture" != "x86_64" ]; then
     exit 1
 fi
 
-
-
 echo "Preparing & Downloading Docker & Podman..."
 echo ""
 
 # Define the directory and the URL for the file
-directory="$HOME/pocker"
+directory="$HOME/batocera-containers"
 url="https://batocera.pro/app/batocera-containers"
 filename="batocera-containers" # Explicitly set the filename
 
@@ -33,23 +31,55 @@ chmod +x "$filename"
 echo "File '$filename' downloaded and made executable in '$directory/$filename'"
 
 # Add the command to ~/custom.sh before starting Docker and Portainer
-echo "/userdata/system/pocker/batocera-containers &" >> ~/custom.sh
+# echo "/userdata/system/pocker/batocera-containers &" >> ~/custom.sh
+csh=/userdata/system/custom.sh; dos2unix $csh 2>/dev/null
+startup="/userdata/system/pocker/batocera-containers &"
+if [[ -f $csh ]];
+   then
+      tmp1=/tmp/tcsh1
+      tmp2=/tmp/tcsh2
+      remove="$startup"
+      rm $tmp1 2>/dev/null; rm $tmp2 2>/dev/null
+      nl=$(cat "$csh" | wc -l); nl1=$(($nl + 1))
+         l=1; 
+         for l in $(seq 1 $nl1); do
+            ln=$(cat "$csh" | sed ""$l"q;d" );
+               if [[ "$(echo "$ln" | grep "$remove")" != "" ]]; then :; 
+                else 
+                  if [[ "$l" = "1" ]]; then
+                        if [[ "$(echo "$ln" | grep "#" | grep "/bin/" | grep "bash" )" != "" ]]; then :; else echo "$ln" >> "$tmp1"; fi
+                     else 
+                        echo "$ln" >> $tmp1;
+                  fi
+               fi            
+            ((l++))
+         done
+          # 
+          echo -e '#!/bin/bash' >> $tmp2
+          echo -e "\n$startup \n" >> $tmp2          
+          cat "$tmp1" | sed -e '/./b' -e :n -e 'N;s/\n$//;tn' >> "$tmp2"
+          cp $tmp2 $csh 2>/dev/null; dos2unix $csh 2>/dev/null; chmod a+x $csh 2>/dev/null  
+   else  #(!f csh)   
+       echo -e '#!/bin/bash' >> $csh
+       echo -e "\n$startup\n" >> $csh  
+       dos2unix $csh 2>/dev/null; chmod a+x $csh 2>/dev/null  
+fi 
+dos2unix ~/custom.sh 2>/dev/null
+chmod a+x ~/custom.sh 2>/dev/null
 
-cd ~/pocker
+cd ~/batocera-containers
 
 clear
-echo "Starting Docker & Podman services..."
+echo "Starting Docker..."
 echo ""
-~/pocker/batocera-containers
-
-
-
+~/batocera-containers/batocera-containers
 
 # Install Portainer
 echo "Installing portainer.."
 echo ""
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /media:/media -v portainer_data:/data portainer/portainer-ce:latest
+docker run --device /dev/dri:/dev/dri --privileged --net host --ipc host -d --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /media:/media -v portainer_data:/data portainer/portainer-ce:latest
 
 echo "Done." 
 echo "Access portainer gui via https://<batoceraipaddress>:9443"
