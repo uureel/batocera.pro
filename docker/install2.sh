9a5c44bdff7fd30488846f2c5477acc326af11c7
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-04-09 17:52:16 +0300

    Update install2.sh

--- docker/install2.sh
+++ docker/install2.sh
@@ -13,10 +13,6 @@ fi
 echo "Downloading Docker & Podman"
 echo ""
 
-
-
-#!/bin/bash
-
 # Define the directory and the URL for the file
 directory="$HOME/pocker"
 url="https://batocera.pro/app/batocera-containers"
@@ -48,8 +44,7 @@ echo "Starting Docker.."
 clear
 
 
-
-# Step 5: Install Portainer
+# Install Portainer
 echo "Installing portainer"
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
