daa8870e0dcf6ace8d554016967221d44b495234
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-04-11 17:36:42 +0300

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -83,14 +83,15 @@ clear
 # Define the options
 OPTIONS=("1" "New Install: Download Arch Container (Older build)"
          "2" "New Install: Build Arch container from scratch (up to date)"
-         "3" "Uninstall Arch Container"
-         "4" "Update Launcher shortcuts for emulationstation Arch container"
-         "5" "Re-download container"
-         "6" "Update apps and rebuild container"
-         "7" "Addon: Add/Update Lutris Menu & Shortcuts to Emulationstation"
-         "8" "Addon: Add/Update Heroic Menu & Shortcuts to Emulationstation"
-         "9" "Addon: Emudeck (experimental)"
-         "10" "Addon: Add/Update Webapps Web/Internet Menu to Emulationstation")
+         "3" "List all Packages in Container"
+         "4" "Uninstall Arch Container"
+         "5" "Update Launcher shortcuts for emulationstation Arch container"
+         "6" "Re-download container"
+         "7" "Update apps and rebuild container"
+         "8" "Addon: Add/Update Lutris Menu & Shortcuts to Emulationstation"
+         "9" "Addon: Add/Update Heroic Menu & Shortcuts to Emulationstation"
+         "10" "Addon: Emudeck (experimental)"
+         "11" "Addon: Add/Update Webapps Web/Internet Menu to Emulationstation")
 
 # Display the dialog and get the user choice
 CHOICE=$(dialog --clear --backtitle "Arch Container Management" \
@@ -119,7 +120,15 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    3)
+     3)
+        echo "Loading Package List..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/list.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    4)
         echo "Loading Uninstall script..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/uninstall.sh
@@ -127,7 +136,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    4)  
+    5)  
         echo "Update EmulationStation Arch Container Launcher Shortcuts..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/update_shortcuts.sh
@@ -135,7 +144,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;    
-    5)  
+    6)  
         echo "Update/Re-download Container..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/redownload.sh
@@ -143,7 +152,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    6)  
+    7)  
         echo "Update Conty Container..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/upgrade.sh
@@ -151,7 +160,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    7)  
+    8)  
         echo "Add/Update Lutris shortcuts to emulationstation..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/addon_lutris.sh
@@ -159,7 +168,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    8)  
+    9)  
         echo "Add/update Heroic shortcuts to emulationstation..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/addon_heroic.sh
@@ -167,7 +176,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    9)  
+    10)  
         echo "Emudeck Menu..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/uureel/batocera.pro/raw/main/emudeck/emudeck.sh
@@ -175,7 +184,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    10)  
+    11)  
         echo "Webapps Installer..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/uureel/batocera.pro/raw/main/webapps/install.sh
