59a508cb9be58e3bd45ce1d1f3e8419bd48e731a
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-04-15 18:55:46 +0300

    Update install_new.sh

--- steam/install_new.sh
+++ steam/install_new.sh
@@ -11,7 +11,7 @@ fi
 
 clear 
 
-MESSAGE="Since Version 39 of Batocera, apparently due to SDL changes, xinput over bluetooth does not work on steam in the Arch container. Xbox One/S/X controllers are verified working via wired USB or Xbox wireless adapter only. 8bitDO controller users can switch their input mode to d-input or switch input.  Continue?"
+MESSAGE="Since ~ v39 of Batocera, apparently due to SDL changes, xinput over bluetooth does not work on steam in the Arch container. Xbox One/S/X controllers are verified working via wired USB or Xbox wireless adapter only. 8bitDO controller users can switch their input mode to d-input or switch input.  Continue?"
 
 # Use dialog to create a yes/no box
 if dialog --title "Compatibility Warning" --yesno "$MESSAGE" 10 70; then
