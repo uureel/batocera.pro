716f4fce86d7e7147428f5a90ae997f6fecded89
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-04-18 23:32:03 +0300

    Update arch-de.sh

--- steam/arch-de.sh
+++ steam/arch-de.sh
@@ -30,10 +30,10 @@ chmod +x ~/services/windowed
 # Download desktop environment scripts to /userdata/desktop and make them executable
 curl -L -o /userdata/desktop/LXDE.sh https://github.com/uureel/batocera.pro/raw/main/steam/desktop/LXDE.sh
 curl -L -o /userdata/desktop/MATE.sh https://github.com/uureel/batocera.pro/raw/main/steam/desktop/MATE.sh
-curl -L -o /userdata/desktop/XFCE.sh https://github.com/uureel/batocera.pro/raw/main/steam/desktop/XFCE.sh
+#curl -L -o /userdata/desktop/XFCE.sh https://github.com/uureel/batocera.pro/raw/main/steam/desktop/XFCE.sh
 chmod +x /userdata/desktop/LXDE.sh
 chmod +x /userdata/desktop/MATE.sh
-chmod +x /userdata/desktop/XFCE.sh
+#chmod +x /userdata/desktop/XFCE.sh
 
 # Display completion message
 dialog --title "Installation Complete" --msgbox " To access desktop mode:\n1. In Emulationstation, Go to Main Menu-->System Settings-->services and toggle on the windowed service. Make sure to select back to save service \n2. Reboot Batocera.\n3. Press F1. You should see a windowed pcmanFM file manager.\n4. In /userdata/desktop, launch one of the desktop environment scripts. \n5. To revert back to regular mode, simply deactivate windowed service and reboot" 12 80
