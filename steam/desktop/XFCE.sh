eff8e46590ba401ca7b150c166ea71f7c9a0902b
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-04-18 11:42:57 +0300

    Add files via upload

---
+++ steam/desktop/XFCE.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#------------------------------------------------
+conty=/userdata/system/pro/steam/conty.sh
+#------------------------------------------------
+batocera-mouse show
+killall -9 steam steamfix steamfixer 2>/dev/null
+#------------------------------------------------
+  "$conty" \
+          --bind /userdata/system/containers/storage /var/lib/containers/storage \
+          --bind /userdata/system/flatpak /var/lib/flatpak \
+          --bind /userdata/system/etc/passwd /etc/passwd \
+          --bind /userdata/system/etc/group /etc/group \
+          --bind /var/run/nvidia /var/run/nvidia \
+          --bind /userdata/system /home/batocera \
+          --bind /sys/fs/cgroup /sys/fs/cgroup \
+          --bind /userdata/system /home/root \
+          --bind /etc/fonts /etc/fonts \
+          --bind /userdata /userdata \
+          --bind /newroot /newroot \
+          --bind / /batocera \
+  bash -c 'prepare && ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1 && dbus-run-session  startxfce4 '"${@}"''
+#------------------------------------------------
+# batocera-mouse hide
+#------------------------------------------------
+							d
