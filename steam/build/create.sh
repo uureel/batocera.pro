1140708e5faa36778a4199a9ceade591524cba42
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-04-22 11:25:00 +0300

    Update create.sh

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -55,14 +55,14 @@ docker docker-compose portainer-bin gthumb doublecmd-qt6 dolphin nmon thunar nem
 gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd krusader mcpelauncher-linux-git krename glances \
 steam-boilr-gui btrfs-assistant protontricks-git lib32-sdl12-compat sdl12-compat  appimagepool-appimage \
 kate kmod pciutils xrdp x11vnc tigervnc remmina vinagre freerdp sunshine btrfs-progs tre screenfetch \
-podman distrobox cheese filezilla dos2unix nix blender wmctrl xorg-xprop fzf scc xorg-server-xwayland \
+podman distrobox cheese filezilla dos2unix nix blender wmctrl xorg-xprop fzf scc  \
 btrfs-heatmap meld lynx yq xfce4-goodies xorg xorg-server-xvfb nodejs npm cairo-dock imagemagick strace \
 tint2 plank lxde mate mate-extra dialog xterm compsize antimicrox qdirstat lutris-git chiaki xfce4 procs \
 protontricks-git chiaki sublime-text-4 heroic-games-launcher-bin moonlight-qt zoom  ventoy-bin 7-zip \
 microsoft-edge-stable-bin qdirstat peazip-gtk2-bin protonup-qt steam-rom-manager-git google-chrome steamtinkerlaunch"
 
 # If you want to install AUR packages, specify them in this variable
-export aur_packagelist="geforcenow-electron freefilesync-bin freerdp-git sgdboop-bin winegui-bin umu-launcher"
+export aur_packagelist="geforcenow-electron freefilesync-bin sgdboop-bin winegui-bin umu-launcher"
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
