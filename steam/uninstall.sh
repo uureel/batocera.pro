e99c480b90844d6abbd7bf3cd045be06b57a6fa1
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-05-03 09:41:16 +0300

    Update uninstall.sh

--- steam/uninstall.sh
+++ steam/uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Introductory message using dialog
-dialog --title "Warning" --msgbox "This script will uninstall the arch container and delete data in the following:\n\n1. ~/.local/share/Conty \n2. ~/pro/steam
-\n3. /userdata/roms/conty \n4. /userdata/roms/lutris \n5. /userdata/roms/heroic2 \n6. /userdata/roms/steam2   \n\nThere may be saves." 15 60
+dialog --title "Warning" --msgbox "This script will uninstall the arch container and delete data ONLY in the following:\n\n1. ~/.local/share/Conty \n2. ~/pro/steam
+\n3. /userdata/roms/conty \n4. /userdata/roms/lutris \n5. /userdata/roms/heroic2 \n6. /userdata/roms/steam2   \n\nThere may be saves. \n\n Data Stored in other locations will NOT be deleted." 15 60
 
 # Confirmation dialog
-if dialog --title "Confirm" --yesno "Do you want to proceed the uninstall and delete the folders? The container's data in ~/pro/steam/home if exists will be deleted!!!  Select 'yes' to continue." 10 60; then
+if dialog --title "Confirm" --yesno "Do you want to proceed the uninstall and delete the folders? Older Installs: The container's data in ~/pro/steam/home if exists will be deleted!!!  Select 'yes' to continue." 10 60; then
     rm -rf ~/pro/steam   
     rm -rf ~/.local/share/Conty
     rm -rf /userdata/roms/conty
@@ -23,7 +23,7 @@ if dialog --title "Confirm" --yesno "Do you want to proceed the uninstall and de
     echo ""
     echo "" 
     sleep 5
-    curl http://127.0.0.1:1234/reloadgames
+    killall -9 emulationstation
 else
     echo "Operation canceled. No folders were deleted."
 fi
