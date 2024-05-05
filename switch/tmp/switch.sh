db36689c060f7d69de203e3ba2bdfea98224ac33
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-05-05 09:34:47 +0300

    Update switch.sh

--- switch/tmp/switch.sh
+++ switch/tmp/switch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash 
-echo "This installer can take a while, be patient"
-sleep 10
-clear
+
 # BATOCERA.PRO INSTALLER
 ######################################################################
 #--------------------------------------------------------------------- 
