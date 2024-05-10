fc4b05cc96444a707aa7e2f5144ae988464d1e6b
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-05-10 16:05:56 +0300

    Create wine.sh

---
+++ wine-custom/wine.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the options
+OPTIONS=("1" "Wine Vanilla and Proton" "2" "Wine-tkg staging")
+
+# Use dialog to display the menu
+CHOICE=$(dialog --clear --backtitle "Wine Installation" \
+                --title "Select a Version..." \
+                --menu "Choose a Wine version to install:" \
+                15 50 4 \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+# Clear the dialog artifacts
+clear
+
+# Run the appropriate script based on the user's choice
+case $CHOICE in
+    1)
+        echo "You chose Wine Vanilla and Proton."
+        curl -L https://github.com/uureel/batocera.pro/raw/main/wine-custom/vanilla.sh | bash
+        ;;
+    2)
+        echo "You chose Wine-tkg staging."
+        curl -L https://github.com/uureel/batocera.pro/raw/main/wine-custom/tkg.sh | bash
+        ;;
+    *)
+        echo "Invalid choice or no choice made. Exiting."
+        ;;
+esac
