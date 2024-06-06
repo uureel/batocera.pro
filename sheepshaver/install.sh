efa532b500f952499c667faf46f91306dd739bc8
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-06 16:27:54 +0300

    +

--- sheepshaver/install.sh
+++ sheepshaver/install.sh
@@ -14,15 +14,17 @@ echo "Download complete."
 
 # Make the AppImage executable
 chmod +x ~/pro/sheepshaver/SheepShaver-x86_64.AppImage
+clear
 
 # Create a launch script in /userdata/roms/ports
 echo "Creating launch script..."
-cat <<EOF > /userdata/roms/ports/sheepshaver.sh
+cat <<EOF > /userdata/roms/ports/SheepShaver.sh
 #!/bin/bash
 DISPLAY=:0.0 ~/pro/sheepshaver/SheepShaver-x86_64.AppImage
 EOF
 
 chmod +x /userdata/roms/ports/sheepshaver.sh
 echo "Installation complete. You can now launch SheepShaver in PORTS"
+sleep 3
 
 curl http://127.0.0.1:1234/reloadgames
