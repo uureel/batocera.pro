0d86b43ab2a5e4e56592577c84e0c30bb2384fd0
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-07 13:39:34 +0300

    Update create.sh

--- steam/build/create.sh
+++ steam/build/create.sh
@@ -50,7 +50,7 @@ xdotool xbindkeys gparted vlc smplayer mpv fish zsh xmlstarlet nvtop duf exa \
 minigalaxy legendary gamescope yt-dlp playonlinux minizip flatpak libreoffice \
 ripgrep i7z sd bandwhich tre zoxide p7zip atop iftop sysstat totem feh krename \
 bottles bauh flatseal rebuild-detector ccache axel breeze xorg-xdpyinfo dua-cli mullvad-browser-bin \
-handbrake tigervnc remmina yt-dlp kitty terminator xorg-xkill media-downloader file discord \
+handbrake tigervnc remmina yt-dlp kitty terminator xorg-xkill media-downloader file discord pcem \
 docker docker-compose portainer-bin unzip gthumb doublecmd-qt6 dolphin nmon thunar nemo konsole \
 gdk-pixbuf-xlib gdk-pixbuf2 xarchiver mc vifm fd krusader mcpelauncher-linux-git krename glances \
 steam-boilr-gui btrfs-assistant protontricks-git lib32-sdl12-compat sdl12-compat  appimagepool-appimage \
