3c9a54f155806361d4e0ec995cc7d5e5788d6cca
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-23 22:48:09 +0300

    Update bliss.sh

--- docker/bliss.sh
+++ docker/bliss.sh
@@ -1,14 +1,9 @@
 echo "Starting Bliss OS...This can take a while..."
 echo "Adjust display to fit to screen in top menu bar settings"
 echo ""
-# Check if the container is already running
-if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-    dialog --title "Container Running" --msgbox "The Bliss OS Docker container is already running." 10 50
-else
-    # Check if the container exists
-    if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-        # Start the existing container
-        docker start $CONTAINER_NAME
+
+CONTAINER_NAME="bliss"
+
 
 docker run -it \
     --privileged \
@@ -23,4 +18,5 @@ docker run -it \
     --device /dev/snd \
     --group-add audio \
     -e EXTRA='-display gtk,gl=on -vga qxl -spice port=5930,disable-ticketing' \
+     --name $CONTAINER_NAME \
     sickcodes/dock-droid:latest
