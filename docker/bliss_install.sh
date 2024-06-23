ca5619ee258bd7bff9e7fb15b977ab737de09ddb
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-23 21:57:31 +0300

    +

--- docker/bliss_install.sh
+++ docker/bliss_install.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 
 # Function to check if a port is in use
 is_port_in_use() {
-    if lsof -i:$1 > /dev/null; then
+    if lsof -i:$1 > /dev/null 2>&1; then
         return 0 # True, port is in use
     else
         return 1 # False, port is not in use
     fi
 }
-
 
 # Check for Docker
 if ! command -v docker &> /dev/null; then
@@ -22,8 +22,23 @@ if ! command -v docker &> /dev/null; then
     fi
 fi
 
-#Bliss Docker config
+# Create directories and download necessary scripts
+mkdir -p ~/pro/bliss
+curl -L https://github.com/uureel/batocera.pro/raw/main/docker/bliss.sh -o ~/pro/bliss/bliss.sh
+chmod +x ~/pro/bliss/bliss.sh
 
+mkdir -p /userdata/roms/ports
+curl -L https://github.com/uureel/batocera.pro/raw/main/docker/Bliss-OS.sh -o /userdata/roms/ports/Bliss-OS.sh
+chmod +x /userdata/roms/ports/Bliss-OS.sh
+
+# Check if port 5930 is in use
+if is_port_in_use 5930; then
+    dialog --title "Port in Use" --msgbox "Port 5930 is already in use. Please free the port and try again." 10 50
+    clear
+    exit 1
+fi
+
+# Bliss Docker config
 docker run -it \
     --privileged \
     --device /dev/kvm \
@@ -35,9 +50,7 @@ docker run -it \
     -e EXTRA='-display gtk,gl=on -vga qxl -spice port=5930,disable-ticketing' \
     sickcodes/dock-droid:latest
 
-
-  
-  # Final dialog message with Portainer management info
+# Final dialog message with Portainer management info
 MSG="Bliss OS Docker container has been set up.\n\nBliss-OS can be run from ports\n\nAdjust other settings in your clients as needed.\n\nThe container can be managed via Portainer web UI on port 9443."
 dialog --title "Bliss OS Setup Complete" --msgbox "$MSG" 20 70
 
