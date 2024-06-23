25c6014f4e86bd785cf7798fe6520998eaca152b
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-23 22:51:23 +0300

    +

--- docker/bliss.sh
+++ docker/bliss.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting Bliss OS...This can take a while..."
-echo "Adjust display to fit to screen in top menu bar settings"
+echo "Adjust display to fit to screen in top menu bar settings after QEMU starts"
 echo ""
 sleep 5
 clear
