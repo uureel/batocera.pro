e40e2d62ff622fc4acbddb41431cdecd2ac70d4d
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-24 00:34:18 +0300

    +

--- docker/bliss.sh
+++ docker/bliss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Starting Bliss OS...This can take a while..."
+echo "Starting Bliss OS Conainer..."
 echo "Adjust display to 'zoom to fit' to screen in top menu bar settings after QEMU starts"
 echo ""
 echo "AFTER THIS WINDOWS CLOSES, press ALT-TAB to switch to QEMU window"
