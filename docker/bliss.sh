fcc7b3c41b88d3f34e1516b1a64efa36be7be351
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-24 00:30:51 +0300

    +

--- docker/bliss.sh
+++ docker/bliss.sh
@@ -3,10 +3,10 @@
 echo "Starting Bliss OS...This can take a while..."
 echo "Adjust display to fit to screen in top menu bar settings after QEMU starts"
 echo ""
+echo "AFTER THIS WINDOWS CLOSES, press ALT-TAB to switch to QEMU window"
 sleep 5
 clear
 
-CONTAINER_NAME="bliss"
 
 
 docker start bliss
