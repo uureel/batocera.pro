c598f76036c9119988bd3b6422ef2ab1d19a4149
uureel <116395185+uureel@users.noreply.github.com>
2024-06-25 16:20:52 +0200

    Update patch.sh

--- steam/build/patch.sh
+++ steam/build/patch.sh
@@ -6,8 +6,9 @@ echo "RUNNING ADDITIONAL BATOCERA CONTY PATCHES"
 echo "-----------------------------------------"
 echo
 
+#--------------------------------------------------------------------------------------------
 # prepare/preload
-	echo "fixing nvidia ld.so.cache"
+	echo -e "\n\n\nfixing nvidia ld.so.cache"
 		rm /usr/bin/prepare 2>/dev/null
 		rm /usr/bin/preload 2>/dev/null
 			wget -q --tries=30 -O /usr/bin/prepare "https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/build/prepare.sh"
@@ -15,8 +16,11 @@ echo
 				chmod 777 /usr/bin/prepare 2>/dev/null
 					cp /usr/bin/prepare /usr/bin/preload 2>/dev/null
 
+#--------------------------------------------------------------------------------------------
 # fix for nvidia lutris
-	echo "fixing lutris"
+	echo -e "\n\n\nfixing lutris"
+		mkdir -p /opt 2>/dev/null 
+		rm -rf /opt/lutris 2>/dev/null
 		cd /opt
 			git clone https://github.com/lutris/lutris
 			sed -i 's,os.geteuid() == 0,os.geteuid() == 888,g' /opt/lutris/lutris/gui/application.py 2>/dev/null
@@ -26,8 +30,9 @@ echo
 				  dos2unix /usr/bin/lutris 2>/dev/null
 				  chmod 777 /usr/bin/lutris 2>/dev/null
 
+#--------------------------------------------------------------------------------------------
 # add ~/.bashrc&profile env
-	echo "fixing .bashrc and .profile"
+	echo -e "\n\n\nfixing .bashrc and .profile"
 		rm ~/.bashrc
 			echo '#!/bin/bash' >> ~/.bashrc
 			echo 'ulimit -H -n 819200 && ulimit -S -n 819200 && sysctl -w fs.inotify.max_user_watches=8192000 vm.max_map_count=2147483642 fs.file-max=8192000 >/dev/null 2>&1' >> ~/.bashrc
@@ -38,6 +43,7 @@ echo
 			echo 'export USER=root' >> ~/.bashrc
 				dos2unix ~/.bashrc 2>/dev/null
 
+#--------------------------------------------------------------------------------------------
 # add fakeid
 	f=/usr/bin/fakeid
 		echo '#!/bin/bash' >> "$f"
@@ -45,15 +51,17 @@ echo
 			dos2unix "$f" 2>/dev/null
 			chmod 777 "$f" 2>/dev/null
 
+#--------------------------------------------------------------------------------------------
 # fix for winestaging bork
-	echo "fixing paths for wine staging"
+	echo -e "\n\n\nfixing paths for wine staging"
 		rm -rf /lib32 2>/dev/null
 		rm -rf /share 2>/dev/null
 			ln -sf /usr/lib32 /lib32
 			ln -sf /usr/share /share
 
+#--------------------------------------------------------------------------------------------
 # fix borked faudio repo
-	echo "fixing faudio staging"
+	echo -e "\n\n\nfixing faudio staging"
 		yes "Y" | pacman -S gstreamer
 		yes "Y" | pacman -S faudio
 			cd /tmp/
@@ -64,8 +72,9 @@ echo
 					yes "Y" | pacman -U "$f" --overwrite='*' && rm "$f"
 			cd ~/
 
+#--------------------------------------------------------------------------------------------
 # add tabby 
-	echo "adding tabby"
+	echo -e "\n\n\nadding tabby"
 		cd /tmp/ 
 			f=/tmp/tabby.pacman
 			link=https://github.com/Eugeny/tabby/releases/download/v1.0.205/tabby-1.0.205-linux-x64.pacman
@@ -73,14 +82,66 @@ echo
 					yes "Y" | pacman -U "$f" --overwrite='*' && rm "$f"
 		cd ~/
 
+#--------------------------------------------------------------------------------------------
 # add nativefier
-	echo "adding nativefier"
+	echo -e "\n\n\nadding nativefier"
 		if [[ "$(which node)" != "" ]] && [[ "$(which npm)" != "" ]]; then
 			npm install -g nativefier
 		fi
 
+#--------------------------------------------------------------------------------------------
+# add fightcade2
+	echo -e "\n\n\nadding fightcade2"
+	pacman -R fightcade2 2>/dev/null
+		link="https://www.fightcade.com/download/linux"
+		p=/opt/fightcade2
+		t=/tmp/fc2
+		f="$t/file"
+			rm -rf $p $t 2>/dev/null
+			mkdir -p $p $t 2>/dev/null
+			cd $t
+			wget -q --show-progress --tries=30 -O "$f" "$link"
+			tar -xf "$f"
+			cd ${PWD}/*/
+			cp -r ${PWD}/* $p/
+			cd $p 
+			sed -i 's,${FC2} --no-sandbox,killall -9 fc2-electron 2>/dev/null \&\& dbus-run-session ${FC2} --no-sandbox,g' $p/Fightcade2.sh
+			rm /usr/bin/fightcade /usr/bin/fightcade2 2>/dev/null
+			rm -rf $t
+				if [[ -f "${p}"/Fightcade2.sh ]]; 
+					then
+					ln -sf "${p}"/Fightcade2.sh /usr/bin/fightcade 2>/dev/null
+					ln -sf "${p}"/Fightcade2.sh /usr/bin/fightcade2 2>/dev/null
+					echo "added fightcade2 latest realease"
+				fi
+
+#--------------------------------------------------------------------------------------------
+# add blender
+	echo -e "\n\n\nadding blender"
+	pacman -R blender blender-bin 2>/dev/null
+		link="https://ftp.nluug.nl/pub/graphics/blender/release/Blender4.1/blender-4.1.1-linux-x64.tar.xz"
+		p=/opt/blender
+		t=/tmp/blender
+		f="$t/file"
+			rm -rf $p $t 2>/dev/null
+			mkdir -p $p $t 2>/dev/null
+			cd $t
+			wget -q --show-progress --tries=30 -O "$f" "$link"
+			tar -xf "$f"
+			cd ${PWD}/*/
+			cp -r ${PWD}/* $p/
+			cd $p 
+			rm /usr/bin/blender 2>/dev/null
+			rm -rf $t
+				if [[ -f "${p}"/blender ]]; 
+					then
+					ln -sf "${p}"/blender /usr/bin/blender 2>/dev/null
+					echo "added blender 4.1.1"
+				fi
+
+#--------------------------------------------------------------------------------------------
 # run additional rootpatches/fixes
-	echo "fixing root apps"
+	echo -e "\n\n\nfixing root apps"
 		sed -i 's,/opt/google/chrome/google-chrome,/opt/google/chrome/google-chrome --no-sandbox --test-type,g' /usr/bin/google-chrome-stable 2>/dev/null
 		sed -i 's,/opt/spotify/spotify,/opt/spotify/spotify --no-sandbox --test-type,g' /usr/bin/spotify 2>/dev/null
 		sed -i '/<description>.*<\/description>/d' /etc/fonts/fonts.conf 2>/dev/null
@@ -93,14 +154,16 @@ echo
 				useradd -r -d /var/lib/libvirt -s /bin/false libvirt-qemu
 				usermod -a -G kvm libvirt-qemu
 
+#--------------------------------------------------------------------------------------------
 # fix samba collisions 
-	echo "fixing samba lockups"
+	echo -e "\n\n\nfixing samba"
 		rm /usr/bin/samba* 2>/dev/null
 		rm /usr/bin/smb* 2>/dev/null
 		rm -rf ~/build 2>/dev/null
 
+#--------------------------------------------------------------------------------------------
 # purge baloo 
-	echo "purging baloo"
+	echo -e "\n\n\npurging baloo"
 		rm /bin/baloo* 2>/dev/null &
 		rm /usr/bin/baloo* 2>/dev/null &
 		rm /usr/lib/baloo* 2>/dev/null &
@@ -115,89 +178,97 @@ echo
 		rm -rf /usr/share/dbus-1/interfaces/org.kde.baloo* 2>/dev/null &
 			wait
 
+#--------------------------------------------------------------------------------------------
 # add appimage greenlight version due to borked yay builder
-	echo "adding appimaged version of greenlight, due to borked yay git builder"
+	echo -e "\n\n\nadding greenlight"
 		link=$(curl -s https://api.github.com/repos/unknownskl/greenlight/releases/latest | jq -r ".assets[] | select(.name | endswith(\".AppImage\")) | .browser_download_url" | grep AppImage)
 			wget --tries=50 --no-check-certificate --no-cache --no-cookies -O "/usr/bin/greenlight-beta" "$link"
 				chmod 777 /usr/bin/greenlight-beta 2>/dev/null
 					ln -sf /usr/bin/greenlight-beta /usr/bin/greenlight 2>/dev/null
-rm $f 2>/dev/null
-rm $h 2>/dev/null
 
-ldconfig
 
-echo
-echo "------"
-echo " DONE"
-echo "------"
-echo
+#--------------------------------------------------------------------------------------------
+	rm $f 2>/dev/null
+	rm $h 2>/dev/null
+
+	ldconfig
+
+	echo
+	echo
+	echo "--------"
+	echo "  DONE"
+	echo "--------"
+	echo
+	echo
 
 exit 0
 
-	### portproton rootpatch 
-	#echo "patching portproton"
-	#cp "$(which portproton)" /usr/bin/properportproton 
-	#wget -q --tries=30 -O /usr/bin/portproton "https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/build/portproton.sh"
-	#dos2unix /usr/bin/portproton 2>/dev/null
-	#chmod 777 /usr/bin/portproton 2>/dev/null
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 
-	### libc dthash patch
-	#echo "fixing libc dthash"
-	#ver=$(ldd --version | head -n1 | rev | awk '{print $1}' | rev)
-	#echo -e "\n\nPREPARING LIBC $ver DT_HASH FIX FOR STEAM...\n\n"
-	#f=/tmp/fixlibc
-	#rm $f 2>/dev/null
-	#echo '#!/bin/bash' >> $f
-	#echo "ver=$ver" >> $f
-	#echo 'mkdir ~/build 2>/dev/null && rm -rf ~/build/glibc && cd ~/build' >> $f
-	#echo 'git clone https://sourceware.org/git/glibc.git ~/build/glibc' >> $f
-	#echo 'cd ~/build/glibc' >> $f
-	#echo "git checkout glibc-$ver" >> $f
-	#echo 'mkdir ~/build/glibc/build && cd ~/build/glibc/build' >> $f
-	#echo 'echo -e "\n\nCONFIGURING...\n\n"' >> $f
-	#echo 'unset LD_LIBRARY_PATH' >> $f
-	#echo 'export CFLAGS="$CFLAGS -O3 -fno-stack-protector -fno-PIC -D_FORTIFY_SOURCE=0"' >> $f
-	#echo 'export LDFLAGS="$LDFLAGS -Wl,--hash-style=both -Wl,-z,norelro"' >> $f
-	#echo 'export LDFLAGS_so="-Wl,--hash-style=both -Wl,-z,norelro"' >> $f
-	#echo 'export LDFLAGS_rtld="-Wl,--hash-style=both -Wl,-z,norelro"' >> $f
-	#echo '../configure \' >> $f
-	#echo '    --prefix=/usr \' >> $f
-	#echo '    --with-headers=/usr/include \' >> $f
-	#echo '    --with-bugurl=https://bugs.archlinux.org/ \' >> $f
-	#echo '    --enable-bind-now \' >> $f
-	#echo '    --enable-cet \' >> $f
-	#echo '    --enable-kernel=4.4 \' >> $f
-	#echo '    --enable-multi-arch \' >> $f
-	#echo '    --disable-stack-protector \' >> $f
-	#echo '    --enable-systemtap \' >> $f
-	#echo '    --disable-crypt \' >> $f
-	#echo '    --disable-profile \' >> $f
-	#echo '    --disable-werror \' >> $f
-	#echo '    --libdir=/usr/lib \' >> $f
-	#echo '    --libexecdir=/usr/lib' >> $f
-	#echo 'echo -e "\n\nCOMPILING...\n\n"' >> $f
-	#echo "make -j$(nproc) 1>/dev/null 2>/dev/null" >> $f
-	#echo 'echo -e "\n\nINSTALLING...\n\n"' >> $f
-	#echo 'sudo make install 1>/dev/null 2>/dev/null' >> $f
-	#echo 'cd ~/' >> $f
-	#echo 'rm -rf ~/build/glibc' >> $f
-	### run libc patcher
-	#dos2unix $f 2>/dev/null
-	#chmod 777 $f 2>/dev/null
-	#/tmp/fixlibc
-	### confirm libc patch status
-	#echo "checking libc patch"
-	#h=/tmp/hash && rm $h 2>/dev/null
-	#readelf -d /usr/lib/libc.so.6 | grep 'HASH' >> $h
-	#	function checklibcpatch() {
-	#		if [[ "$(cat $h | grep '(HASH)')" != "" ]] && [[ "$(cat $h | grep '(GNU_HASH)')" != "" ]]; then
-	#			echo
-	#			echo "LIBC DT_HASH PATCHED OK!"
-	#			echo
-	#		else
-	#			echo
-	#			echo "LIBC DT_HASH PATCH FAILED..."
-	#			echo	
-	#		fi
-	#	}
-	#	checklibcpatch
+function libc-dthash-patch() {
+	echo "fixing libc dthash"
+	ver=$(ldd --version | head -n1 | rev | awk '{print $1}' | rev)
+	echo -e "\n\nPREPARING LIBC $ver DT_HASH FIX FOR STEAM...\n\n"
+	# prepare libc patcher
+	f=/tmp/fixlibc
+	rm $f 2>/dev/null
+	echo '#!/bin/bash' >> $f
+	echo "ver=$ver" >> $f
+	echo 'mkdir ~/build 2>/dev/null && rm -rf ~/build/glibc && cd ~/build' >> $f
+	echo 'git clone https://sourceware.org/git/glibc.git ~/build/glibc' >> $f
+	echo 'cd ~/build/glibc' >> $f
+	echo "git checkout glibc-$ver" >> $f
+	echo 'mkdir ~/build/glibc/build && cd ~/build/glibc/build' >> $f
+	echo 'echo -e "\n\nCONFIGURING...\n\n"' >> $f
+	echo 'unset LD_LIBRARY_PATH' >> $f
+	echo 'export CFLAGS="$CFLAGS -O3 -fno-stack-protector -fno-PIC -D_FORTIFY_SOURCE=0"' >> $f
+	echo 'export LDFLAGS="$LDFLAGS -Wl,--hash-style=both -Wl,-z,norelro"' >> $f
+	echo 'export LDFLAGS_so="-Wl,--hash-style=both -Wl,-z,norelro"' >> $f
+	echo 'export LDFLAGS_rtld="-Wl,--hash-style=both -Wl,-z,norelro"' >> $f
+	echo '../configure \' >> $f
+	echo '    --prefix=/usr \' >> $f
+	echo '    --with-headers=/usr/include \' >> $f
+	echo '    --with-bugurl=https://bugs.archlinux.org/ \' >> $f
+	echo '    --enable-bind-now \' >> $f
+	echo '    --enable-cet \' >> $f
+	echo '    --enable-kernel=4.4 \' >> $f
+	echo '    --enable-multi-arch \' >> $f
+	echo '    --disable-stack-protector \' >> $f
+	echo '    --enable-systemtap \' >> $f
+	echo '    --disable-crypt \' >> $f
+	echo '    --disable-profile \' >> $f
+	echo '    --disable-werror \' >> $f
+	echo '    --libdir=/usr/lib \' >> $f
+	echo '    --libexecdir=/usr/lib' >> $f
+	echo 'echo -e "\n\nCOMPILING...\n\n"' >> $f
+	echo "make -j$(nproc) 1>/dev/null 2>/dev/null" >> $f
+	echo 'echo -e "\n\nINSTALLING...\n\n"' >> $f
+	echo 'sudo make install 1>/dev/null 2>/dev/null' >> $f
+	echo 'cd ~/' >> $f
+	echo 'rm -rf ~/build/glibc' >> $f
+	# run libc patcher
+	dos2unix $f 2>/dev/null
+	chmod 777 $f 2>/dev/null
+	/tmp/fixlibc
+		# confirm libc patch status
+		echo "checking libc patch"
+		h=/tmp/hash && rm $h 2>/dev/null
+		readelf -d /usr/lib/libc.so.6 | grep 'HASH' >> $h
+			function checklibcpatch() {
+				if [[ "$(cat $h | grep '(HASH)')" != "" ]] && [[ "$(cat $h | grep '(GNU_HASH)')" != "" ]]; then
+					echo
+					echo "LIBC DT_HASH PATCHED OK!"
+					echo
+				else
+					echo
+					echo "LIBC DT_HASH PATCH FAILED..."
+					echo	
+				fi
+			}
+			checklibcpatch
+}
+#--------------------------------------------------------------------------------------------
+#libc-dthash-patch 
+#--------------------------------------------------------------------------------------------
