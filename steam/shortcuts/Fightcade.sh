2a5fb399396259f0cb510a3b153d2233a6793d09
uureel <116395185+uureel@users.noreply.github.com>
2024-06-26 16:27:57 +0200

    Update Fightcade.sh

--- steam/shortcuts/Fightcade.sh
+++ steam/shortcuts/Fightcade.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #------------------------------------------------
+function update_launcher() {
+  tmp=/tmp/.fc && rm $tmp 2>/dev/null
+  launcher="/userdata/system/pro/steam/batocera-fightcade.sh"
+  link="https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/shortcuts/batocera-fightcade.sh"
+    wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O "$tmp" "$link"
+      if [[ -s "$tmp" ]]; then cp "$tmp" "$launcher" && rm "$tmp"; else exit 1; fi
+        chmod 777 "$launcher" 2>/dev/null && dos2unix "$launcher" 2>/dev/null
+}
+update_launcher
+#------------------------------------------------
 conty=/userdata/system/pro/steam/conty.sh
 #------------------------------------------------
 batocera-mouse show
@@ -23,7 +33,7 @@ batocera-mouse show
           --bind /userdata/roms/snes /opt/fightcade2/emulator/snes9x/ROMs \
           --bind /userdata/roms/fbneo /opt/fightcade2/emulator/fbneo/ROMs \
           --bind /userdata/roms/dreamcast /opt/fightcade2/emulator/flycast/ROMs \
-  bash -c 'prepare && dbus-run-session /opt/fightcade2/fc2-electron/fc2-electron --no-sandbox'
+  bash -c 'prepare && cp /userdata/system/pro/steam/batocera-fightcade.sh /opt/fightcade2/batocera-fightcade.sh && dbus-run-session /opt/fightcade2/batocera-fightcade.sh'
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
