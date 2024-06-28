fc55fe2d36429e717e5dbe5dbce97c97bf3f6c0a
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-06-28 15:42:24 +0300

    +

--- applewin/applewin.sh
+++ applewin/applewin.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Colors for fancy output
+# Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# Function to display a fancy animation title
+# Function to display title
 function fancy_title() {
     clear
     echo -e "${RED}#####################################${NC}"
