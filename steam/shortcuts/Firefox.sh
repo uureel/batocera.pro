bef49efb6a6692e2e755fb4951a76d299edc34de
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:07:15 +0200

    Update Firefox.sh

--- steam/shortcuts/Firefox.sh
+++ steam/shortcuts/Firefox.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session /usr/lib/firefox/firefox '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session /usr/lib/firefox/firefox '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
