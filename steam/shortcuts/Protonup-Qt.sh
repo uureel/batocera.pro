987dd418d9031d722f13a1657aedeff4ab155a34
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:13:40 +0200

    Update Protonup-Qt.sh

--- steam/shortcuts/Protonup-Qt.sh
+++ steam/shortcuts/Protonup-Qt.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session protonup-qt '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session protonup-qt '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
