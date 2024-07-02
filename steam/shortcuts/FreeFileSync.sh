dea2e8ccae707cc73c0756034807ef5f4383833c
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:07:38 +0200

    Update FreeFileSync.sh

--- steam/shortcuts/FreeFileSync.sh
+++ steam/shortcuts/FreeFileSync.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session /opt/freefilesync/FreeFileSync '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session /opt/freefilesync/FreeFileSync '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
