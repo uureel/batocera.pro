bcc82be8300eb74dec040565f55459ed12978e08
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:10:41 +0200

    Update Kodi.sh

--- steam/shortcuts/Kodi.sh
+++ steam/shortcuts/Kodi.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session kodi '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session kodi '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
