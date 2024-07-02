dcd7d466fd07f2d5265f5d8e2ebb74b801cece25
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:10:18 +0200

    Update Inkscape.sh

--- steam/shortcuts/Inkscape.sh
+++ steam/shortcuts/Inkscape.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session inkscape '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session inkscape '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
