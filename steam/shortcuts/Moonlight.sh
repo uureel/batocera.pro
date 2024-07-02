8ba52efab2ea4554016d239d395017a36401d7f8
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:11:41 +0200

    Update Moonlight.sh

--- steam/shortcuts/Moonlight.sh
+++ steam/shortcuts/Moonlight.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && moonlight '"${@}"''
+  bash -c 'prepare && source /opt/env && moonlight '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
