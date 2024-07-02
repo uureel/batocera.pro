a596508e41adebc8be75365117034f990abcd66d
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:09:26 +0200

    Update Gthumb.sh

--- steam/shortcuts/Gthumb.sh
+++ steam/shortcuts/Gthumb.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session gthumb '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session gthumb '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
