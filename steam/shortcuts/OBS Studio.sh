cd9f1a30fc6123480ae682f87546b1642c839153
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:12:28 +0200

    Update OBS Studio.sh

--- steam/shortcuts/OBS Studio.sh
+++ steam/shortcuts/OBS Studio.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session obs '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session obs '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
