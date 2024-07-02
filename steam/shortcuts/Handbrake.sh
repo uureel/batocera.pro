f691df458d56942085cb389151a3b3edc95c754d
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:09:38 +0200

    Update Handbrake.sh

--- steam/shortcuts/Handbrake.sh
+++ steam/shortcuts/Handbrake.sh
@@ -17,7 +17,7 @@ killall -9 steam steamfix steamfixer 2>/dev/null
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && dbus-run-session ghb '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session ghb '"${@}"''
 #------------------------------------------------
 batocera-mouse hide
 #------------------------------------------------
