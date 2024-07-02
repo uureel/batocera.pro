fd0289e25d4c87c800a9d8b0c0a8942799f8df19
uureel <116395185+uureel@users.noreply.github.com>
2024-07-02 20:17:02 +0200

    Update Terminal-Terminator.sh

--- steam/shortcuts/Terminal-Terminator.sh
+++ steam/shortcuts/Terminal-Terminator.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && terminator '"${@}"''
+  bash -c 'prepare && source /opt/env && terminator '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
