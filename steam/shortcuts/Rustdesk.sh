d0a1a4dc297221c0c4b423494d68e45a13a0c3c0
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-07-03 16:28:50 +0300

    +

---
+++ steam/shortcuts/Rustdesk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#------------------------------------------------
+conty=/userdata/system/pro/steam/conty.sh
+#------------------------------------------------
+batocera-mouse show
+#------------------------------------------------
+  "$conty" \
+          --bind /userdata/system/containers/storage /var/lib/containers/storage \
+          --bind /userdata/system/flatpak /var/lib/flatpak \
+          --bind /userdata/system/etc/passwd /etc/passwd \
+          --bind /userdata/system/etc/group /etc/group \
+          --bind /var/run/nvidia /var/run/nvidia \
+          --bind /userdata/system /home/batocera \
+          --bind /sys/fs/cgroup /sys/fs/cgroup \
+          --bind /userdata/system /home/root \
+          --bind /etc/fonts /etc/fonts \
+          --bind /userdata /userdata \
+          --bind /newroot /newroot \
+          --bind / /batocera \
+  bash -c 'prepare && source /opt/env && dbus-run-session rustdesk '"${@}"''
+#------------------------------------------------
+# batocera-mouse hide
+#------------------------------------------------
+
