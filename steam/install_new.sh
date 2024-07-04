437ca5ae31b0618cc9a826455ce9d839c6bea5ef
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-07-04 23:18:44 +0300

    +

--- steam/install_new.sh
+++ steam/install_new.sh
@@ -11,6 +11,20 @@ fi
 
 clear 
 
+MESSAGE="This container is compatible with EXT4 or BTRFS partitions only!  NTFS/EXFat are not supported.  Continue?"
+
+# Use dialog to create a yes/no box
+if dialog --title "Compatibility Warning" --yesno "$MESSAGE" 10 70; then
+    # If the user chooses 'Yes', continue the installation
+    echo "Continuing installation..."
+    # Add your installation commands here
+else
+    # If the user chooses 'No', exit the script
+    echo "Installation aborted by user."
+    exit 1
+fi
+
+
 MESSAGE="Since ~ v39 of Batocera, apparently due to SDL changes, xinput over bluetooth does not work on steam in the Arch container. Xbox One/S/X controllers are verified working via wired USB or Xbox wireless adapter only. 8bitDO controller users can switch their input mode to d-input or switch input.  Continue?"
 
 # Use dialog to create a yes/no box
