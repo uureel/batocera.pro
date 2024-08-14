5c34dbb399e9b5f0ef5159bb2330e65225a8fdce
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-08-14 14:48:22 +0300

    +

--- steam/steam.sh
+++ steam/steam.sh
@@ -24,7 +24,7 @@ OPTIONS=("1" "Install STABLE: Download prebuilt Arch Container (RECOMMENDED)"
          "10" "Addon: Add/Update Heroic Menu & Shortcuts to Emulationstation"
          "11" "Addon: Emudeck (experimental)"
          "12" "Addon: Nativefier (turn websites into apps and add to ES Menu)"
-         "13" "Addon: NVIDIA CUDA")
+         "13" "Addon: NVIDIA CUDA Support")
 
 # Display the dialog and get the user choice
 CHOICE=$(dialog --clear --backtitle "Arch Container Management" \
