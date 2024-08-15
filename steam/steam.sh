3aa1f61d0a0a38ba4ad83f470596e07c32c1f1a4
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-08-15 11:51:15 +0300

    +

--- steam/steam.sh
+++ steam/steam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Part 1: Display initial message
-dialog --msgbox "Note for Steam: the official flatpak version of Steam is now working on v40.  Data is stored in /userdata/saves/flatpak/data/.var/app/com.valvesoftware.Steam/data/Steam \n\nRegular Proton builds have been verified working and the flatpak build is a smaller install.\n\nThe Arch container build supports xdg and launching Heroic, Lutris, and other games via Steam plus tools like protontricks and protonup-qt are included. \n\nthe Arch container stores steam data in ~/.local/share/Steam with a total size of around 5GB." 20 70
+dialog --msgbox "Note for Steam: the official flatpak version of Steam is now working on v40.  Data is stored in /userdata/saves/flatpak/data/.var/app/com.valvesoftware.Steam/data/Steam \n\nRegular Proton builds have been verified working and the flatpak build is a smaller install.\n\nThe Arch container build supports xdg and launching Heroic, Lutris, and other games via Steam plus tools like protontricks and protonup-qt are included. \n\nthe Arch container stores steam data in ~/.local/share/Steam and the container size is around 5GB." 20 70
 
 # Get the machine hardware name
 architecture=$(uname -m)
