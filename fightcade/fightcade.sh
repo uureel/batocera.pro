b3d901d937ccac94a1e5f52f620cbda80f50150e
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-08-15 10:31:33 +0300

    +

--- fightcade/fightcade.sh
+++ fightcade/fightcade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Fightcade has now been merged into the arch container.  Please install the arch container for fightcade"
-sleep 5
+echo "Fightcade has now been merged into the arch container.  Please install the arch container bleeding edge for fightcade"
+sleep 8
 curl -L arch.batocera.pro | bash
