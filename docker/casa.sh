2aa485ddab224b5fb9f5d6e7440a67d7f92675a0
Cliffy <162914836+garbagescow@users.noreply.github.com>
2024-09-04 14:25:13 +0300

    Update casa.sh

--- docker/casa.sh
+++ docker/casa.sh
@@ -29,10 +29,10 @@ fi
 
 # Download the split zip files with aria2c
 echo "Downloading split zip files using aria2c..."
-./aria2c "${ZIP_PART_1}" -o "batocera-casaos.tar.zip.001"
-./aria2c "${ZIP_PART_2}" -o "batocera-casaos.tar.zip.002"
-./aria2c "${ZIP_PART_3}" -o "batocera-casaos.tar.zip.003"
-./aria2c "${ZIP_PART_4}" -o "batocera-casaos.tar.zip.004"
+./aria2c "${ZIP_PART_1}" -x 5-o "batocera-casaos.tar.zip.001"
+./aria2c "${ZIP_PART_2}" -x 5-o "batocera-casaos.tar.zip.002"
+./aria2c "${ZIP_PART_3}" -x 5-o "batocera-casaos.tar.zip.003"
+./aria2c "${ZIP_PART_4}" -x 5-o "batocera-casaos.tar.zip.004"
 
 if [ $? -ne 0 ]; then
     echo "Failed to download one or more parts of the split zip file. Exiting."
@@ -57,7 +57,7 @@ fi
 
 # Extract the tar.gz file
 echo "Extracting the tar.gz file..."
-tar -xf "batocera-casaos.tar"
+tar -xvzf "batocera-casaos.tar.gz"
 if [ $? -ne 0 ]; then
     echo "Failed to extract the tar.gz file. Exiting."
     exit 1
